1815848faf5e51d51113031c65c3cfa5071bfa4d
keertip <keertip@google.com>
2013-10-07 12:51:43 -0700

    add a file open action

--- app/spark.dart
+++ app/spark.dart
@@ -9,7 +9,9 @@ import 'dart:html';
 
 import 'package:chrome/app.dart' as chrome;
 
+import 'lib/ace.dart';
 import 'lib/utils.dart';
+
 
 void main() {
   Spark spark = new Spark();
@@ -17,20 +19,26 @@ void main() {
 
 class Spark {
 
+  var editor;
+
   Spark() {
     document.title = appName;
     print(appName);
 
-    ParagraphElement p = new ParagraphElement();
-    p.innerHtml = appName;
-    p.style.fontSize = '36pt';
-    p.style.marginTop = '100px';
-    p.style.textAlign = 'center';
-    p.style.color = '#999999';
-    p.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
-    p.style.textShadow = '0 1px 2px';
-    document.body.children.add(p);
+//    ParagraphElement p = new ParagraphElement();
+//    p.innerHtml = appName;
+//    p.style.fontSize = '36pt';
+//    p.style.marginTop = '100px';
+//    p.style.textAlign = 'center';
+//    p.style.color = '#999999';
+//    p.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
+//    p.style.textShadow = '0 1px 2px';
+//    document.body.children.add(p);
 
+    query("#openFile")
+    ..onClick.listen(openFile);
+
+    editor = new AceEditor();
     chrome.app.window.current.onClosed.listen(handleWindowClosed);
   }
 
@@ -39,5 +47,15 @@ class Spark {
   void handleWindowClosed(data) {
     // TODO:
     //print('handleWindowClosed');
+  }
+
+  void openFile(MouseEvent event){
+    chrome.fileSystem.chooseEntry(type: 'openWritableFile').then((chrome.FileEntry file) {
+      if (file != null) {
+        file.readText().then((String contents) {
+          editor.setContent(file);
+        });
+      }
+    });
   }
 }
