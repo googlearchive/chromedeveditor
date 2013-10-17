3827fa1447d520079023aaed3500837f477c814b
Gaurav Agarwal <grv@chromium.org>
2013-10-17 15:38:25 -0700

    move sha1 and delta module out of git.js.

--- app/git/git.js
+++ app/git/git.js
@@ -419,112 +419,7 @@ var requirejs, require, define;
 
 define("thirdparty/almond", function(){});
 
-define('objectstore/delta',[],function() {
-    var applyDelta = (function() {
-        var matchLength = function(stream) {
-            var data = stream.data
-            var offset = stream.offset
-            var result = 0
-            var currentShift = 0
-            var _byte = 128
-            var maskedByte, shiftedByte
-
-            while ((_byte & 128) != 0) {
-                _byte = data[offset]
-                offset += 1
-                maskedByte = _byte & 0x7f
-                shiftedByte = maskedByte << currentShift
-                result += shiftedByte
-                currentShift += 7
-            }
-            stream.offset = offset
-            return result
-        }
-
-        return function(baseData, delta) {
-            //var baseData = Git.stringToBytes(baseDataString)
-            var stream = {
-                data: delta,
-                offset: 0,
-                length: delta.length
-            }
-            var bb = [];
-            var baseLength = matchLength(stream)
-            if (baseLength != baseData.length) {
-                throw (Error("Delta Error: base length not equal to length of given base data"))
-            }
-
-            var resultLength = matchLength(stream)
-            var resultData = new Uint8Array(resultLength);
-            var resultOffset = 0;
-
-            var copyOffset
-            var copyLength
-            var opcode
-            var copyFromResult
-            while (stream.offset < stream.length) {
-                opcode = stream.data[stream.offset]
-                stream.offset += 1
-                copyOffset = 0
-                copyLength = 0
-                if (opcode == 0) {
-                    throw (Error("Don't know what to do with a delta opcode 0"))
-                } else if ((opcode & 0x80) != 0) {
-                    var value
-                    var shift = 0
-                    _(4).times(function() {
-                        if ((opcode & 0x01) != 0) {
-                            value = stream.data[stream.offset]
-                            stream.offset += 1
-                            copyOffset += (value << shift)
-                        }
-                        opcode >>= 1
-                        shift += 8
-                    })
-                    shift = 0
-                    _(2).times(function() {
-                        if ((opcode & 0x01) != 0) {
-                            value = stream.data[stream.offset]
-                            stream.offset += 1
-                            copyLength += (value << shift)
-                        }
-                        opcode >>= 1
-                        shift += 8
-                    })
-                    if (copyLength == 0) {
-                        copyLength = (1 << 16)
-                    }
-
-                    // TODO: check if this is a version 2 packfile and apply copyFromResult if so
-                    copyFromResult = (opcode & 0x01)
-                    var subarray = baseData.subarray(copyOffset, copyOffset + copyLength);
-                    resultData.set(subarray, resultOffset);
-                    resultOffset += subarray.length;
-
-                } else if ((opcode & 0x80) == 0) {
-                    var subarray = stream.data.subarray(stream.offset, stream.offset + opcode);
-                    resultData.set(subarray, resultOffset);
-                    resultOffset += subarray.length;
-                    stream.offset += opcode
-                }
-            }
-            return resultData.buffer;
-
-        }
-    }());
-
-    return applyDelta;
-});
-if(typeof Crypto=="undefined"||!Crypto.util)(function(){var i=window.Crypto={},l=i.util={rotl:function(a,c){return a<<c|a>>>32-c},rotr:function(a,c){return a<<32-c|a>>>c},endian:function(a){if(a.constructor==Number)return l.rotl(a,8)&16711935|l.rotl(a,24)&4278255360;for(var c=0;c<a.length;c++)a[c]=l.endian(a[c]);return a},randomBytes:function(a){for(var c=[];a>0;a--)c.push(Math.floor(Math.random()*256));return c},bytesToWords:function(a){for(var c=[],b=0,d=0;b<a.length;b++,d+=8)c[d>>>5]|=a[b]<<24-
-d%32;return c},wordsToBytes:function(a){for(var c=[],b=0;b<a.length*32;b+=8)c.push(a[b>>>5]>>>24-b%32&255);return c},bytesToHex:function(a){for(var c=[],b=0;b<a.length;b++){c.push((a[b]>>>4).toString(16));c.push((a[b]&15).toString(16))}return c.join("")},hexToBytes:function(a){for(var c=[],b=0;b<a.length;b+=2)c.push(parseInt(a.substr(b,2),16));return c},bytesToBase64:function(a){if(typeof btoa=="function")return btoa(m.bytesToString(a));for(var c=[],b=0;b<a.length;b+=3)for(var d=a[b]<<16|a[b+1]<<
-8|a[b+2],e=0;e<4;e++)b*8+e*6<=a.length*8?c.push("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charAt(d>>>6*(3-e)&63)):c.push("=");return c.join("")},base64ToBytes:function(a){if(typeof atob=="function")return m.stringToBytes(atob(a));a=a.replace(/[^A-Z0-9+\/]/ig,"");for(var c=[],b=0,d=0;b<a.length;d=++b%4)d!=0&&c.push(("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".indexOf(a.charAt(b-1))&Math.pow(2,-2*d+8)-1)<<d*2|"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".indexOf(a.charAt(b))>>>
-6-d*2);return c}};i.mode={};i=i.charenc={};i.UTF8={stringToBytes:function(a){return m.stringToBytes(unescape(encodeURIComponent(a)))},bytesToString:function(a){return decodeURIComponent(escape(m.bytesToString(a)))}};var m=i.Binary={stringToBytes:function(a){for(var c=[],b=0;b<a.length;b++)c.push(a.charCodeAt(b)&255);return c},bytesToString:function(a){for(var c=[],b=0;b<a.length;b++)c.push(String.fromCharCode(a[b]));return c.join("")}}})();
-(function(){var i=Crypto,l=i.util,m=i.charenc,a=m.UTF8,c=m.Binary,b=i.SHA1=function(d,e){var g=l.wordsToBytes(b._sha1(d));return e&&e.asBytes?g:e&&e.asString?c.bytesToString(g):l.bytesToHex(g)};b._sha1=function(d){if(d.constructor==String)d=a.stringToBytes(d);var e=l.bytesToWords(d),g=d.length*8;d=[];var n=1732584193,h=-271733879,j=-1732584194,k=271733878,o=-1009589776;e[g>>5]|=128<<24-g%32;e[(g+64>>>9<<4)+15]=g;for(g=0;g<e.length;g+=16){for(var q=n,r=h,s=j,t=k,u=o,f=0;f<80;f++){if(f<16)d[f]=e[g+
-f];else{var p=d[f-3]^d[f-8]^d[f-14]^d[f-16];d[f]=p<<1|p>>>31}p=(n<<5|n>>>27)+o+(d[f]>>>0)+(f<20?(h&j|~h&k)+1518500249:f<40?(h^j^k)+1859775393:f<60?(h&j|h&k|j&k)-1894007588:(h^j^k)-899497514);o=k;k=j;j=h<<30|h>>>2;h=n;n=p}n+=q;h+=r;j+=s;k+=t;o+=u}return[n,h,j,k,o]};b._blocksize=16;b._digestsize=20})();
-
-define("thirdparty/2.2.0-sha1", function(){});
-
-define('formats/pack',['objectstore/delta', 'thirdparty/2.2.0-sha1'], function(applyDelta) {
+define('formats/pack',[], function() {
 
     String.prototype.rjust = function(width, padding) {
         padding = padding || " ";
@@ -3437,7 +3332,7 @@ GitLiteWorkerMessages = {
 define("workers/worker_messages", function(){});
 
 
-define('api',['commands/clone', 'commands/commit', 'commands/init', 'commands/pull', 'commands/push', 'commands/branch', 'commands/checkout', 'commands/conditions', 'objectstore/file_repo', 'formats/smart_http_remote', 'utils/errors', 'thirdparty/2.2.0-sha1', 'thirdparty/crc32', "workers/worker_messages"], function(clone, commit, init, pull, push, branch, checkout, Conditions, FileObjectStore, SmartHttpRemote, errutils){
+define('api',['commands/clone', 'commands/commit', 'commands/init', 'commands/pull', 'commands/push', 'commands/branch', 'commands/checkout', 'commands/conditions', 'objectstore/file_repo', 'formats/smart_http_remote', 'utils/errors', 'thirdparty/crc32', "workers/worker_messages"], function(clone, commit, init, pull, push, branch, checkout, Conditions, FileObjectStore, SmartHttpRemote, errutils){
 
     /** @exports GitApi */
     var api = {
