6895fb086bdd51b3ae46ef2a864ab5b9c482cd4b
Hoa V. Dinh <dvh@google.com>
2013-10-17 23:47:05 -0700

    Fixed FileItemView warnings

--- app/lib/file_item_view.dart
+++ app/lib/file_item_view.dart
@@ -4,10 +4,8 @@ class FileItemView {
   Element _element;
   
   FileItemView(String path) {
-    Element template = document.query('#fileview-filename').content;
-    //children[0];
+    DocumentFragment template = query('#fileview-filename').content;
     _element = template.clone(true);
-    //print(_element.outerHtml);
     _element.query('.filename').text = path;
     query('#fileViewArea').children.add(_element);
   }
