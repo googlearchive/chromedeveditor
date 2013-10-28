c6b655712bf5a5d1e98170215ba9359faae69114
Sergey Shevchenko <sergeygs@google.com>
2013-10-28 02:34:32 -0700

    Small non-functional changes/fixes

--- widgets/lib/src/spark-button.dart
+++ widgets/lib/src/spark-button.dart
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-library spark.widgets.button;
+library spark_widgets.button;
 
 import 'package:polymer/polymer.dart';
 
