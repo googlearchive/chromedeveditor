dc32cea5f0c91b34410575b012ef35f23f2ecabc
Sergey Shevchenko <sergeygs@google.com>
2013-11-02 00:46:44 -0700

    Added build.dart to make Polymer iterations in Dart Editor easier.
    Essentially just pulls in the changes under widgets/ into ide/app/packages, allowing quick debugging of the uncompiled app.
    
    Still needs to be run manually every time 'widgets/' change, but that can be done via the context menu or Run button of the editor without switching to command line and back.

---
+++ ide/build.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:grinder/grinder.dart' as grind;
+import 'package:polymer/builder.dart' as polymer;
+
+import 'tool/grind.dart' as grind;
+
+void main() {
+  new grind.Grinder()
+    ..addTask(new grind.GrinderTask(
+        'update', taskFunction: update))
+    ..addTask(new grind.GrinderTask(
+        'lint', taskFunction: lint, depends: ['update']))
+    ..start(['update', 'lint']);
+  exitCode = 1;
+}
+
+void update(context) {
+  grind.setup(context);
+}
+
+void lint(context) {
+  polymer.lint(entryPoints: ['app/spark_polymer.html']);
+}
