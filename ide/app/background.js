850521a795f7deb8a56004e9a216e376058bd9e4
Sergey Shevchenko <sergeygs@google.com>
2013-11-02 01:28:05 -0700

    Added custom context menu to Spark + do UI switching using it
    Replaced the not-so-pretty links directly in the app's UI with a context menu item.
    
    Note: there seems to be a race condition somewhere: when repeatedly switching from one UI to the other multiple timees, eventually the UI alternate windows stop opening/closing in a random fashion. 'this.window' in the code becomes null. This function is dev-only, though, and the first time switching always work,  so don't spend too much effort now figuring it out.

--- ide/app/background.js
+++ ide/app/background.js
@@ -38,7 +38,7 @@ App.prototype.updateSettings = function(changedSettings) {
 };
 
 App.prototype.launch = function(skin_opt) {
-  if (skin_opt) {
+  if (skin_opt !== undefined) {
     this.updateSettings({ skin: skin_opt });
   }
 
@@ -99,11 +99,21 @@ EditorWindow.prototype.onLoad_ = function() {
   if (uiSwitcher) {
     uiSwitcher.onclick = this.app_.switchUi.bind(this.app_);
   }
+
+  chrome.contextMenus.create({
+    title: "Switch UI",
+    id: 'switch_ui',
+    contexts: [ 'all' ]
+  });
+  chrome.contextMenus.onClicked.addListener(function(info) {
+    theApp.switchUi();
+  });
 }
 
-// Destroy the window.
+// Destroy the window, if any.
 EditorWindow.prototype.destroy = function() {
-  this.window.close();
+  if (this.window)
+    this.window.close();
 }
 
 // Create a new app window and restore settings/state.
