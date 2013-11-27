72b051a93488f8fa143edca37e0ca33fa3e6872b
Sergey Shevchenko <sergeygs@google.com>
2013-11-26 16:05:46 -0800

    Nit

--- widgets/lib/spark-button/spark-button.dart
+++ widgets/lib/spark-button/spark-button.dart
@@ -1,5 +1,5 @@
 // Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSDstyle
+// All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 library spark_widgets.button;
