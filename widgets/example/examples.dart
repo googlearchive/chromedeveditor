fa0d12068c22062d96749f93b021330f16f3cb4b
Adam Singer <financeCoding@gmail.com>
2013-12-05 22:27:47 -0800

    Clean up examples and added index page

---
+++ widgets/example/examples.dart
@@ -0,0 +1,25 @@
+library examples;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('widget-examples')
+class Examples extends PolymerElement {
+  final links = ['spark-button',
+                 'spark-icon',
+                 'spark-icon-button',
+                 'spark-menu',
+                 'spark-menu-button',
+                 'spark-menu-item',
+                 'spark-overlay',
+                 'spark-select',
+                 'spark-selection',
+                 'spark-selector',
+                 'spark-splitter',
+                 'spark-togglebutton',
+                 'spark-toolbar'];
+
+  factory Examples() => new Element.tag('Examples');
+
+  Examples.created() : super.created();
+}
