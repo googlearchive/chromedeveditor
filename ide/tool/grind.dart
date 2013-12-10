d72b5800d4c806d0c92719dfc6904339e035e576
devoncarew@gmail.com <devoncarew@gmail.com>
2013-12-10 09:09:39 -0800

    upgrade to path 1.0.0

--- ide/tool/grind.dart
+++ ide/tool/grind.dart
@@ -249,6 +249,8 @@ void _dart2jsCompile(GrinderContext context, Directory target, String filePath,
   runSdkBinary(context, 'dart2js', arguments: [
      joinDir(target, [filePath]).path,
      '--package-root=packages',
+     '--suppress-warnings',
+     '--suppress-hints',
      '--out=' + joinDir(target, ['${filePath}.js']).path]);
 
   // clean up unnecessary (and large) files
