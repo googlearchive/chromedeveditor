2afc634225e1d2acb54de0605f9985c554c4aae5
Hoa V. Dinh <dvh@google.com>
2013-12-18 11:00:48 -0800

    Remove key "key" from the version for WebStore

--- ide/tool/grind.dart
+++ ide/tool/grind.dart
@@ -118,12 +118,16 @@ void release(GrinderContext context) {
     return;
   }
 
-  String version = _increaseBuildNumber(context);
+  String version = _increaseBuildNumber(context, removeKey: true);
   // Creating an archive of the Chrome App.
   context.log('Creating build ${version}');
 
   archive(context);
 
+  _runCommandSync(
+    context,
+    'git checkout app/manifest.json');
+  _increaseBuildNumber(context);
   _runCommandSync(
     context,
     'git commit -m "Build version ${version}" app/manifest.json');
@@ -320,7 +324,7 @@ void _archiveWithRevision(GrinderContext context) {
 
 // Increase the build number in the manifest.json file. Returns the full
 // version.
-String _increaseBuildNumber(GrinderContext context) {
+String _increaseBuildNumber(GrinderContext context, {bool removeKey: false}) {
   // Tweaking build version in manifest.
   File file = new File('app/manifest.json');
   String content = file.readAsStringSync();
@@ -337,6 +341,9 @@ String _increaseBuildNumber(GrinderContext context) {
 
   version = '${majorVersion}.${buildVersion}';
   manifestDict['version'] = version;
+  if (removeKey) {
+    manifestDict.remove('key');
+  }
   file.writeAsStringSync(new JsonPrinter().print(manifestDict));
 
   // It needs to be copied to compile result directory.
