8ccccd0b6bee16d1d81e625499ecd3f184083717
Hoa V. Dinh <dvh@google.com>
2014-01-02 15:49:12 -0800

    Fixed style

--- ide/tool/grind.dart
+++ ide/tool/grind.dart
@@ -24,13 +24,13 @@ void main([List<String> args]) {
   defineTask('compile', taskFunction: compile, depends : ['setup']);
   defineTask('deploy', taskFunction: deploy, depends : ['setup']);
 
-  defineTask('docs', taskFunction : docs, depends : ['setup']);
+  defineTask('docs', taskFunction: docs, depends : ['setup']);
   defineTask('stats', taskFunction: stats);
-  defineTask('archive', taskFunction : archive, depends : ['mode-notest', 'compile']);
+  defineTask('archive', taskFunction: archive, depends : ['mode-notest', 'compile']);
   
   // For now, we won't be building the webstore version from Windows.
   if (!Platform.isWindows) {
-    defineTask('release', taskFunction : release, depends : ['mode-notest', 'compile']);
+    defineTask('release', taskFunction: release, depends : ['mode-notest', 'compile']);
     defineTask('release-nightly',
                taskFunction : releaseNighly,
                depends : ['mode-notest', 'compile']);
