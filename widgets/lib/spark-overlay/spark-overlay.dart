0619cd0d9a4ddac08eeeea95bcfae5709ad7117a
Sungguk Lim <limasdf@gmail.com>
2014-01-04 06:34:19 +0900

    rename focus attribute

--- widgets/lib/spark-overlay/spark-overlay.dart
+++ widgets/lib/spark-overlay/spark-overlay.dart
@@ -139,7 +139,7 @@ class SparkOverlay extends Widget {
   }
 
   getFocusNode() {
-    var focus = this.querySelector('[autofocus]');
+    var focus = this.querySelector('[focused]');
     return (focus != null) ? focus : this;
   }
 
