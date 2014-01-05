8b91f31801d7cc1aa6d09d124fdd679e4eac9d70
Sungguk Lim <limasdf@gmail.com>
2014-01-05 23:49:54 +0900

    do nothing when outside of modal clicked

--- widgets/lib/spark-modal/spark-modal.dart
+++ widgets/lib/spark-modal/spark-modal.dart
@@ -4,6 +4,7 @@
 
 library spark_widgets.menu;
 
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 
 import '../spark-overlay/spark-overlay.dart';
@@ -13,4 +14,12 @@ import '../spark-overlay/spark-overlay.dart';
 @CustomTag("spark-modal")
 class SparkModal extends SparkOverlay {
   SparkModal.created(): super.created();
+
+  @override
+  void captureHandler(MouseEvent e) {
+    if (!pointInOverlay(this, e.client)) {
+      e.stopImmediatePropagation();
+      e.preventDefault();
+    }
+  }
 }
