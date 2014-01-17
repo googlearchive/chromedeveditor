db57376a547f6f13dbdc7237ced9bee48c7647fc
Cameron Fitzgerald <camfitz@google.com>
2014-01-17 17:04:46 +1100

    Basic implementation of checkForUncommittedChanges.

--- ide/app/lib/git/commands/conditions.dart
+++ ide/app/lib/git/commands/conditions.dart
@@ -8,14 +8,28 @@ import 'dart:async';
 
 import 'package:chrome/chrome_app.dart' as chrome;
 
+import 'commit.dart';
 import '../objectstore.dart';
 
 class Conditions {
 
   static Future<GitConfig> checkForUncommittedChanges(chrome.DirectoryEntry dir,
       ObjectStore store) {
-    //TODO(grv) : implement.
-    return new Future.value();
+
+    return store.getHeadRef().then((String headRefName) {
+      return store.getHeadForRef(headRefName).then((String parent) {
+        return Commit.walkFiles(dir, store).then((String sha) {
+          return Commit.checkTreeChanged(store, parent, sha).then((_) {
+            // hasChanges true
+          }, onError: (e) {
+            if (e == "commits_no_changes")
+              // hasChanges false
+            else
+              throw e;
+          });
+        });
+      });
+    });
   }
 
 }
