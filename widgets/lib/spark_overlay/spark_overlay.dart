af5c23ec32b6ef0de7e7d93d6e073e28ca7669f0
ussuri <sergeygs@google.com>
2014-01-20 01:47:51 -0800

    Use more correct boudning rectangle in SparkOverlay.pointInOverlay() + nits
    In particular, this fixes the problem where a click on a <button> or other plain HTML element in spark-overlay would close it (with autoCloseDisabled == false).

--- widgets/lib/spark_overlay/spark_overlay.dart
+++ widgets/lib/spark_overlay/spark_overlay.dart
@@ -230,7 +230,7 @@ class SparkOverlay extends SparkWidget {
   // scrim.
   void captureHandler(MouseEvent e) {
     // TODO(terry): Hack to work around lightdom or event.path not yet working.
-    if (!autoCloseDisabled && !pointInOverlay(this, e.client)) {
+    if (!autoCloseDisabled && !isPointInOverlay(e.client)) {
       // TODO(terry): How to cancel the event e.cancelable = true;
       e.stopImmediatePropagation();
       e.preventDefault();
@@ -239,8 +239,8 @@ class SparkOverlay extends SparkWidget {
     }
   }
 
-  bool pointInOverlay(SparkOverlay overlay, Point xyGlobal) {
-    return overlay.offset.containsPoint(xyGlobal);
+  bool isPointInOverlay(Point xyGlobal) {
+    return super.getBoundingClientRect().containsPoint(xyGlobal);
   }
 
   void keydownHandler(KeyboardEvent e) {
