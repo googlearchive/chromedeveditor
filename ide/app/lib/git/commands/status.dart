16d87fccecff973f538361df6af215d43656b3a5
Gaurav Agarwal <grv@chromium.org>
2014-01-20 23:34:13 -0800

    Add git status.

---
+++ ide/app/lib/git/commands/status.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+library git.commands.status;
+
+import 'dart:async';
+
+import 'package:chrome/chrome_app.dart' as chrome;
+
+import 'index.dart';
+import '../objectstore.dart';
+import '../utils.dart';
+
+class Status {
+
+  static Future<Map<String, StatusEntry>> getFileStatuses(ObjectStore store) {
+    return store.index.updateIndex().then((_) {
+      return store.index.statusMap;
+    });
+  }
+
+  static Future<StatusEntry> getFileStatus(ObjectStore store,
+      chrome.ChromeFileEntry entry) {
+    Completer completer = new Completer();
+    entry.getMetadata().then((data) {
+      // TODO(grv) : check the modification time when it is available.
+      getShaForEntry(entry, 'blob').then((String sha) {
+        StatusEntry status = new StatusEntry(entry.fullPath, sha, data.size);
+        store.index.updateIndexForEntry(status);
+        completer.complete(store.index.getStatusForEntry(entry));
+      });
+    });
+    return completer.future;
+  }
+}
