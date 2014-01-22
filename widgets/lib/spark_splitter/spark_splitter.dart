9716abb52817931db5aa0da31e808015613ec872
ussuri <sergeygs@google.com>
2014-01-22 10:22:20 -0800

    Call super.enteredView() in enterView override
    It seemed to work without this, but this is a prescription coming from Polymer itself, so we'd better follow.

--- widgets/lib/spark_splitter/spark_splitter.dart
+++ widgets/lib/spark_splitter/spark_splitter.dart
@@ -78,6 +78,8 @@ class SparkSplitter extends SparkWidget {
   /// Triggered when the control is first displayed.
   @override
   void enteredView() {
+    super.enteredView();
+
     // TODO(sergeygs): Perhaps switch to using onDrag* instead of onMouse* once
     // support for drag-and-drop in shadow DOM is fixed. It is less important
     // here, because the element is not actually supposed to be dropped onto
