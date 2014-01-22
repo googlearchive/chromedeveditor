5011f9f9712e4143db7bd30f99c33cc5e4a50274
Eric Arnold <ericarnold@google.com>
2014-01-22 10:04:13 -0800

    First stab at spark-menu-separator files (will probably remove these) and inclusion into example.

---
+++ widgets/lib/spark_menu_separator/spark_menu_separator.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+library spark_widgets.menu_item;
+
+import 'package:polymer/polymer.dart';
+
+import '../common/spark_widget.dart';
+
+// Ported from Polymer Javascript to Dart code.
+
+@CustomTag("spark-menu-separator")
+class SparkMenuItem extends SparkWidget {
+  /// URL image for the icon associated with this menu item.
+  @published String src = "";
+
+  /// Size of the icon.
+  @published String iconsize = "24";
+
+  /// Specifies the label for the menu item.
+  @published String label = "";
+
+  /// Specifies the label for the menu item.
+  @published bool separator = false;
+
+  SparkMenuItem.created() : super.created() {
+  }
+
+  void enteredView() {
+    if (separator) {
+      this.classes.add("separator");
+    }
+  }
+}
