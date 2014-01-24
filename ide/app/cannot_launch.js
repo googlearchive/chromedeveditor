d9c320d2cb42d1b55a15a5315ca76bc00afe5d89
Hoa V. Dinh <dvh@google.com>
2014-01-23 16:30:09 -0800

    Moved script tag to head

--- ide/app/cannot_launch.js
+++ ide/app/cannot_launch.js
@@ -2,6 +2,8 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-document.querySelector('#close-button').addEventListener('click', function() {
-  window.close();
-});
+onload = function() {
+  document.querySelector('#close-button').addEventListener('click', function() {
+    window.close();
+  });
+};
