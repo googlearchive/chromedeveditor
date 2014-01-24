e790e321b5005b2220cc286f6454fcac331f3e47
Hoa V. Dinh <dvh@google.com>
2014-01-23 16:25:48 -0800

    Added copyright headers

--- ide/app/cannot_launch.js
+++ ide/app/cannot_launch.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 document.querySelector('#close-button').addEventListener('click', function() {
   window.close();
 });
