49385aad0fded0dbddcef01bb654283da32cab06
Eric Arnold <ericarnold@google.com>
2014-01-28 12:23:29 -0800

    Added Serialization and wrote CustomRule.  Will revert as we decided not to use Serialization package.

--- ide/app/services/services.dart
+++ ide/app/services/services.dart
@@ -1,6 +1,7 @@
+import 'package:serialization/serialization.dart';
+
 import 'dart:isolate';
 import 'dart:async';
-
 
 class Services {
   final String workerPath = 'services/services_isolate.dart';
@@ -14,14 +15,45 @@ class Services {
         sendPort = parameter;
       } else {
         String message = parameter;
-        print('message\n');
+        print('$message\n');
       }
     });
 
     Uri workerUri = Uri.parse(workerPath);
 
     Isolate.spawnUri(workerUri, [], receivePort.sendPort).then((isolate) {
-      Timer.run(() => sendPort.send('From app: "Marco!"'));
+      Say command = new Say();
+      command.message = 'Marco';
+      // TODO(ericarnold): Sending shared-code object instances is supported
+      //    only by dartvm not by dart2js.  If / when that changes, we can
+      //    remove Serialization dependency.
+      //    See https://api.dartlang.org/docs/channels/stable/latest/dart_isolate/SendPort.html#send
+      var serialization = new Serialization()
+          ..addRule(new SayRule());
+      Map output = serialization.write(command);
+
+      Timer.run(() => sendPort.send(output));
     });
+  }
+}
+
+abstract class Command {
+}
+
+class Say extends Command{
+  String message;
+  int volume;
+  String toString() {
+    return message + ((volume >= 11) ? "!" : "");
+  }
+}
+
+class SayRule extends CustomRule {
+  bool appliesTo(instance, Writer w) => instance.runtimeType == Say;
+  getState(Say instance) => [instance.message, instance.volume];
+  create(state) => new Say();
+  setState(Say a, List state) {
+    a.message = state[0];
+    a.volume = state[1];
   }
 }
--- ide/app/services/services_isolate.dart
+++ ide/app/services/services_isolate.dart
@@ -1,10 +1,31 @@
 import 'dart:isolate';
 
+import 'package:serialization/serialization.dart';
+
+import 'services.dart';
+
 main(List<String> args, SendPort sendPort) {
+  final servicesIsolate = new ServicesIsolate();
+
   ReceivePort receivePort = new ReceivePort();
   sendPort.send(receivePort.sendPort);
 
   receivePort.listen((message) {
+    message.
+//    switch (message) {
+//      // TODO(ericarnold): Testing.  Remove.
+//      case "say":
+//        servicesIsolate.say(mesage);
+//    }
     sendPort.send('$message - From isolate: "Polo!"');
   });
+}
+
+class ServicesIsolate {
+  ServicesIsolate();
+
+  // TODO(ericarnold): Testing.  Remove.
+  say(String message) {
+    return "Polo!";
+  }
 }
