ea7b55d4abf6f2f3447991647cd504de773d23cb
Cameron Fitzgerald <camfitz@google.com>
2014-01-29 16:25:54 +1100

    Adding paging functionality for log.

--- ide/app/lib/git/commands/log.dart
+++ ide/app/lib/git/commands/log.dart
@@ -6,32 +6,51 @@ library git.commands.log;
 
 import 'dart:async';
 
+import '../object.dart';
 import '../objectstore.dart';
 
+/**
+ * State based implementation of Log. Repeated calls to log will get parent
+ * commit objects in order to avoid retrieving the entire log in a single call.
+ */
 class Log {
+  List<String> _nextHeadSha;
+
   /**
    * Returns a list of CommitObjects.
    * store: ObjectStore from which to get commits.
-   * branch: Branch from which to get commits. If empty
-   * length: Maximum number of commits to list.
-   *
-   * TODO (camfitz): Add offset to skip the first 'n' commits.
+   * branch: Branch from which to get commits. If null or empty the current
+   * HEAD will be used.
+   * length: Maximum number of commits to list. If null all commits will be
+   * listed.
    */
-  static Future log(ObjectStore store, int length, [String branch]) {
-    Future listCommits(String headSha) {
-      return store.getCommitGraph([headSha], length).then(
-          (CommitGraph graph) => graph.commits);
+  Future<List<CommitObject>> log(ObjectStore store, int length, [String branch]) {
+    Future<List<CommitObject>> listCommits(List<String> headSha) {
+      return store.getCommitGraph(headSha, length).then(
+          (CommitGraph graph) {
+        List<CommitObject> commits = graph.commits;
+        if (commits.length > 0) {
+          CommitObject next = commits[commits.length - 1];
+          _nextHeadSha = next.parents;
+        }
+        return commits;
+      });
     }
 
+    if (_nextHeadSha != null && length != null)
+      return listCommits(_nextHeadSha);
+
     if (branch == null || branch.isEmpty) {
-      return store.getHeadSha().then(listCommits);
+      return store.getHeadSha().then(
+          (String headSha) => listCommits([headSha]));
     }
     return store.getAllHeads().then((List<String> branches) {
       if (!branches.contains(branch)) {
         // TODO: Improve error handling.
         throw "No such branch exists.";
       }
-      return store.getHeadForRef('refs/heads/$branch').then(listCommits);
+      return store.getHeadForRef('refs/heads/$branch').then(
+          (String headSha) => listCommits([headSha]));
     });
   }
 }
