e92882ea7723810d5764c88069d43df881ca79f6
Eric Arnold <ericarnold@google.com>
2014-01-29 09:53:28 -0800

    Review Fixes

--- ide/app/services/services_isolate.dart
+++ ide/app/services/services_isolate.dart
@@ -1,13 +1,22 @@
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+library spark.services;
+
 import 'dart:isolate';
 
 import 'services.dart';
 
-main(List<String> args, SendPort sendPort) {
-  final servicesIsolate = new ServicesIsolate(sendPort);
+void main(List<String> args, SendPort sendPort) {
+  final ServicesIsolate servicesIsolate = new ServicesIsolate(sendPort);
 }
 
+/**
+ * This is a separate application spawned by Spark (via Services) as an isolate
+ * for use in running long-running / heaving tasks.
+ */
 class ServicesIsolate {
-  SendPort sendPort;
+  final SendPort sendPort;
   ServiceHandler handler;
 
   ServicesIsolate(this.sendPort) {
