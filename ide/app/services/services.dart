9e4a5bff7ef93cf24878beb3ac9fc147e44e5586
Eric Arnold <ericarnold@google.com>
2014-01-28 23:14:53 -0800

    Handling class instantiation and repeater with common services class and service handler

--- ide/app/services/services.dart
+++ ide/app/services/services.dart
@@ -5,40 +5,77 @@ import 'dart:convert';
 
 class Services {
   final String workerPath = 'services/services_isolate.dart';
+  ServiceHandler handler;
   SendPort sendPort;
+  final ReceivePort receivePort = new ReceivePort();
 
   Services() {
-    final ReceivePort receivePort = new ReceivePort();
+    handler = new ServiceHandler(receivePort);
+    handler.listenForSendPort(workerPath).then((_){
+      new Timer.periodic(const Duration(seconds: 1), (t) {
+        print("Sending Message!");
+        TestMessage command = new TestMessage.yell('Marco');
 
-    receivePort.listen((parameter) {
+        handler.sendCommand(command);
+      });
+    });
+  }
+}
+
+
+class ServiceHandler {
+  SendPort sendPort;
+  ReceivePort receivePort;
+
+  Future listenForSendPort(String workerPath) {
+    Completer completer = new Completer();
+    receivePort.listen((msg) {
+      /*%TRACE3*/ print("(4> 1/28/14): listen!"); // TRACE%
       if (sendPort == null) {
-        sendPort = parameter;
+        sendPort = msg;
+        completer.complete();
       } else {
-        String message = parameter;
-        print('$message\n');
+        performCommand(msg);
       }
     });
 
     Uri workerUri = Uri.parse(workerPath);
 
-    Isolate.spawnUri(workerUri, [], receivePort.sendPort).then((isolate) {
-      TestMessage command = new TestMessage('Marco')
-        ..volume = 11;
+    Isolate.spawnUri(workerUri, [], receivePort.sendPort);
+    return completer.future;
+  }
 
-      Timer.run(() => sendCommand(command));
-    });
+  ServiceHandler(this.receivePort, [this.sendPort]) {
+    if (sendPort != null) {
+      receivePort.listen((command) {
+        performCommand(command);
+      });
+    }
   }
 
   sendCommand(Command command) {
     sendPort.send({"id": command.commandId, "data": command.serialize()});
   }
+
+  void performCommand(command) {
+    String commandId = command["id"];
+    String serializedData = command["data"];
+
+    switch (commandId) {
+      // TODO(ericarnold): Testing.  Remove.
+      case "message":
+        sendCommand(new TestMessage.serialized(serializedData).respond());
+        break;
+    }
+  }
 }
 
 abstract class Command {
-  String get commandId;
-  String serialize();
   Command();
   Command.serialized(String serializedData);
+  String get commandId;
+  String serialize();
+  Command respond();
 }
 
 // TODO(ericarnold): Testing.  Remove.
@@ -46,12 +83,19 @@ class TestMessage extends Command{
   String message;
   int volume;
 
-  TestMessage(this.message);
+  TestMessage();
+  TestMessage.yell(this.message);
 
   TestMessage.serialized(String serializedData) {
     var data = JSON.decode(serializedData);
-    this.message = data.message;
-    this.volume = data.volume;
+    this.message = data['message'];
+    this.volume = data['volume'];
+  }
+
+  TestMessage respond() {
+    if (message == 'Marco!') {
+      return new TestMessage.yell('Polo');
+    }
   }
 
   String get commandId => "message";
