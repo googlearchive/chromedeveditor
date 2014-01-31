b65deb857fab0ec86dfa1870bb14612f4c8139f0
Eric Arnold <ericarnold@google.com>
2014-01-31 14:43:18 -0800

    Revert "TEMP Removed other tests"
    This reverts commit 0055e680efc9582202e4cb078d05719dd4639c0e.

--- ide/app/test/all.dart
+++ ide/app/test/all.dart
@@ -7,11 +7,51 @@
  */
 library spark.all_tests;
 
+import 'ace_test.dart' as ace_test;
+import 'actions_test.dart' as actions_test;
+import 'analytics_test.dart' as analytics_test;
+import 'analyzer_test.dart' as analyzer_test;
+import 'app_test.dart' as app_test;
+import 'builder_test.dart' as builder_test;
+import 'compiler_test.dart' as compiler_test;
+import 'editors_test.dart' as editors_test;
+import 'event_bus_test.dart' as event_bus_test;
+import 'files_test.dart' as files_test;
+import 'jobs_test.dart' as jobs_test;
+import 'git/all.dart' as git_all_test;
+import 'preferences_test.dart' as preferences_test;
+import 'scm_test.dart' as scm_test;
+import 'sdk_test.dart' as sdk_test;
+import 'search_test.dart' as search_test;
+import 'server_test.dart' as server_test;
 import 'services_test.dart' as services_test;
+import 'tcp_test.dart' as tcp_test;
+import 'utils_test.dart' as utils_test;
+import 'workspace_test.dart' as workspace_test;
 
 /**
  * Place all new tests here.
  */
 void defineTests() {
+  ace_test.defineTests();
+  actions_test.defineTests();
+  analytics_test.defineTests();
+  analyzer_test.defineTests();
+  app_test.defineTests();
+  builder_test.defineTests();
+  compiler_test.defineTests();
+  editors_test.defineTests();
+  event_bus_test.defineTests();
+  files_test.defineTests();
+  jobs_test.defineTests();
+  git_all_test.defineTests();
+  preferences_test.defineTests();
+  scm_test.defineTests();
+  sdk_test.defineTests();
+  search_test.defineTests();
+  server_test.defineTests();
   services_test.defineTests();
+  tcp_test.defineTests();
+  utils_test.defineTests();
+  workspace_test.defineTests();
 }
