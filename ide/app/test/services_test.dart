03c4591b90d2f70a0689f76ee9e82d814c47c355
Eric Arnold <ericarnold@google.com>
2014-01-31 14:34:34 -0800

    Added tests and changed library name for services_impl.  Added mock send port for services_impl test to check status with.

--- ide/app/test/services_test.dart
+++ ide/app/test/services_test.dart
@@ -1,34 +1,48 @@
-// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 library spark.services_test;
 
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:unittest/unittest.dart';
 
 import '../lib/services/services.dart';
+import '../services_impl.dart';
 
 Services services;
+ServicesIsolate servicesIsolate;
 
 defineTests() {
   group('services', () {
-    //
-
     setUp(() {
       services = new Services();
     });
 
-//    tearDown(() {
-//
-//    }
-
     test('ping', () {
-      services.ping().then((result) {
-        expect("abc", equals("abc"));
+      return services.ping().then((result) {
+        expect(result, equals("pong"));
       });
     });
   });
+
+  group('services_impl', () {
+    test('setup', () {
+      MockSendPort mockSendPort = new MockSendPort();
+      servicesIsolate = new ServicesIsolate(mockSendPort);
+      expect(mockSendPort.wasSent, isNotNull);
+    });
+  });
+
 }
 
+class MockSendPort extends SendPort {
+  bool operator ==(other) => super == other;
+  int get hashCode => null;
+  var wasSent = null;
+  void send(var message) {
+    wasSent = message;
+  }
+}
