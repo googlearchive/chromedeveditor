1a893ff7bcf8bce69435829cbfdc6d55111fcc88
ussuri <sergeygs@google.com>
2014-02-03 02:10:46 -0800

    Implement spark-split-view
    Easier to work with than naked spark-splitter - fewer styles to set on the client side, especially tricky-to-get-right flex.

---
+++ widgets/lib/spark_split_view/spark_split_view.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+library spark_widgets.split_view;
+
+import 'package:polymer/polymer.dart';
+
+import '../common/spark_widget.dart';
+import '../spark_splitter/spark_splitter.dart';
+
+@CustomTag('spark-split-view')
+class SparkSplitView extends SparkWidget {
+  /// These are just wires into the enclosed [SparkSplitter]. See that class
+  /// for the description of the attributes.
+  @published String direction = 'left';
+  @published int splitterSize = 8;
+  @published bool splitterHandle = true;
+  @published bool locked = false;
+  @published SplitterUpdateFunction onUpdate;
+
+  /// Constructor.
+  SparkSplitView.created() : super.created();
+
+  /**
+   * Set the current splitter location.
+   */
+  set targetSize(num val) {
+    ($['splitter'] as SparkSplitter).targetSize = val;
+  }
+}
