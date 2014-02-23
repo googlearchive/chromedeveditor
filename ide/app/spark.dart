93eead23c36d6e9fb4ef33a5806a0a04f1d7672a
Sungguk Lim <limasdf@gmail.com>
2014-02-24 01:56:33 +0900

    nit feedback

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -700,7 +700,7 @@ class ProjectLocationManager {
 
       _projectLocation = new LocationResult(entry, entry, false);
       _prefs.setValue('projectFolder', chrome.fileSystem.retainEntry(entry));
-      return new LocationResult(entry, entry, false);
+      return _projectLocation;
     });
   }
 
