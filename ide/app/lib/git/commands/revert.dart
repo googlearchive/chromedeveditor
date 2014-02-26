7eac768e041b92a29e7b3b0fe6dae82e9b8a605c
Gaurav Agarwal <grv@chromium.org>
2014-02-26 13:44:02 -0800

    comments.

--- ide/app/lib/git/commands/revert.dart
+++ ide/app/lib/git/commands/revert.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -16,7 +16,7 @@ import 'status.dart';
 
 
 /**
- * Reverts a given file [entry] to the head state.
+ * Reverts a given list of file [entries] to the git head state.
  */
 class Revert {
   static Future revert(GitOptions options, List<chrome.ChromeFileEntry>
