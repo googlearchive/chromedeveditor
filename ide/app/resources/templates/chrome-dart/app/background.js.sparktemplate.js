0e6faa3d9c7f09dc1c742679ef7b1ad38c4cde93
Eric Arnold <ericarnold@google.com>
2014-02-26 12:58:50 -0800

    Removed dart chrome app template

--- ide/app/resources/templates/chrome-dart/app/background.js.sparktemplate.js
+++
@@ -1,6 +0,0 @@
-
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('_source_name_.html', {
-    'id': '_mainWindow', 'bounds': {'width': 800, 'height': 600 }
-  });
-});
