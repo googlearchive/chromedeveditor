de8fcdeb22a1573e1f5cee1d6d54b693bab28e16
Sungguk Lim <limasdf@gmail.com>
2014-03-14 23:22:52 +0900

    Add filesize and last modified time at properties dialog

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -10,6 +10,7 @@ import 'dart:html' hide File;
 
 import 'package:bootjack/bootjack.dart' as bootjack;
 import 'package:chrome/chrome_app.dart' as chrome;
+import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
 
@@ -1740,6 +1741,17 @@ class PropertiesAction extends SparkActionWithDialog implements ContextAction {
               '<error retrieving Git data>');
         });
       }
+    }).then((_) {
+      _selectedResource.entry.getMetadata().then((meta) {
+        if (_selectedResource.entry is FileEntry) {
+          String size = new NumberFormat.decimalPattern().format(meta.size);
+          _addProperty(_propertiesElement, 'Size', '$size bytes');
+        }
+
+        String lastModified =
+            new DateFormat.yMd().add_Hm().format(meta.modificationTime);
+        _addProperty(_propertiesElement, 'Last Modified', lastModified);
+      });
     });
   }
 
