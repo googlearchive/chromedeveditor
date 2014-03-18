31de0d737b1b8b6669099334facab54e0953c462
Hoa V. Dinh <dvh@google.com>
2014-03-18 14:57:14 -0700

    Revert "Merge pull request #1247 from devoncarew/devoncarew_git_archive_inflate"
    This reverts commit bbb2866634376c0f3b0c46618a5cdc62e4f3d0b1, reversing
    changes made to 71f3f868f0efb6e8ffaaf18396303064d0e5589e.
    
    Conflicts:
    	ide/app/test/benchmarks.dart
    	ide/pubspec.yaml

--- ide/app/test/git/zlib_test.dart
+++ ide/app/test/git/zlib_test.dart
@@ -14,17 +14,19 @@ import '../../lib/utils.dart';
 import '../../lib/git/zlib.dart';
 
 final String ZLIB_INPUT_STRING = "This is a test.";
-final String ZLIB_DEFLATE_OUTPUT = "12011120120044860162681331462122261861041151536";
+final String ZLIB_DEFLATE_OUTPUT =
+  "12015613194651300821924236481415120184233272346311218722219302251454341151536";
 
 defineTests() {
   group('git.zlib', () {
     test('inflate', () {
       // deflate a string.
       Uint8List buffer = new Uint8List.fromList(encodeUtf8(ZLIB_INPUT_STRING));
-      Uint8List deflated = Zlib.deflate(buffer);
+      ZlibResult deflated = Zlib.deflate(buffer);
+      Uint8List buffer2 = new Uint8List.fromList(deflated.data);
 
       // inflate the string back.
-      ZlibResult result = Zlib.inflate(deflated);
+      ZlibResult result = Zlib.inflate(buffer2);
 
       String out = UTF8.decode(result.data);
       expect(out, ZLIB_INPUT_STRING);
@@ -32,8 +34,10 @@ defineTests() {
 
     test('deflate', () {
       Uint8List buffer = new Uint8List.fromList(encodeUtf8(ZLIB_INPUT_STRING));
-      List<int> result = Zlib.deflate(buffer);
-      expect(result.join(''), ZLIB_DEFLATE_OUTPUT);
+      ZlibResult result = Zlib.deflate(buffer);
+
+      List<int> bytes = result.data;
+      expect(bytes.join(''), ZLIB_DEFLATE_OUTPUT);
     });
 
     // This test will time out if zlib deflate is very slow.
@@ -50,9 +54,9 @@ defineTests() {
       String str = buf.toString();
       Uint8List data = new Uint8List.fromList(UTF8.encoder.convert(str));
       timer.finishCurrentTask('encode');
-      List<int> resultBytes = Zlib.deflate(data);
+      ZlibResult result = Zlib.deflate(data);
       timer.finishCurrentTask('deflate');
-      ZlibResult result = Zlib.inflate(resultBytes);
+      result = Zlib.inflate(result.data);
       timer.finishCurrentTask('inflate');
       String decodedString = UTF8.decoder.convert(result.data);
       timer.finishCurrentTask('decode');
