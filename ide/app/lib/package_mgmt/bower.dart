7260d134d6c00f30a757e30be4016411079f98c2
ussuri <sergeygs@google.com>
2014-04-11 02:21:40 -0700

    Transitive Bower dependency downloading v.2.0
    Downloads in 2 stages:
    1) Recursively XHRs all bower.json's starting at the project's top bower.json. Record dependencies for every one.
    2) Downloads the resulting set of dependencies in parallel.

--- ide/app/lib/package_mgmt/bower.dart
+++ ide/app/lib/package_mgmt/bower.dart
@@ -11,14 +11,12 @@
 library spark.package_mgmt.bower;
 
 import 'dart:async';
-import 'dart:html' as html;
 import 'dart:convert' show JSON;
 
-import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:logging/logging.dart';
 
 import 'package_manager.dart';
-import '../scm.dart';
+import 'bower_fetcher.dart';
 import '../workspace.dart';
 
 Logger _logger = new Logger('spark.bower');
@@ -58,9 +56,10 @@ class BowerManager extends PackageManager {
 
     return project.getOrCreateFolder(properties.packagesDirName, true)
         .then((Folder packagesDir) {
-      final _BowerFetcher fetcher = new _BowerFetcher(packagesDir.entry);
+      final fetcher = new BowerFetcher(
+          packagesDir.entry, properties.packageSpecFileName);
 
-      return fetcher.getDependencies(specFile.entry).whenComplete(() {
+      return fetcher.fetchDependencies(specFile.entry).whenComplete(() {
         return project.refresh();
       }).catchError((e, st) {
         _logger.severe('Error getting Bower packages', e, st);
@@ -71,76 +70,6 @@ class BowerManager extends PackageManager {
 
   Future upgradePackages(Project project) {
     return new Future.error('Not implemented');
-  }
-}
-
-class _BowerFetcher {
-  static const _GITHUB_ROOT_URL = 'https://github.com';
-  static const _GITHUB_USER_CONTENT_URL = 'https://raw.githubusercontent.com';
-  /// E.g.: "Polymer/polymer-elements".
-  static const _PACKAGE_SPEC_PATH = '''[-\\w./]+''';
-  /// E.g.: "#master", "#1.2.3".
-  static const _PACKAGE_SPEC_BRANCH = '''[-\\w.]+''';
-  /// E.g.: "Polymer/polymer#master";
-  static final RegExp _PACKAGE_SPEC_REGEXP =
-      new RegExp('''^($_PACKAGE_SPEC_PATH)(?:#($_PACKAGE_SPEC_BRANCH))?\$''');
-
-  static final ScmProvider _scmProvider = getProviderType('git');
-  chrome.DirectoryEntry _packagesDir;
-
-  _BowerFetcher(this._packagesDir);
-
-  Future getDependencies(chrome.ChromeFileEntry specFile) {
-    return specFile.readText().then((String spec) {
-      final Map<String, dynamic> specMap = JSON.decode(spec);
-      final Map<String, String> deps = specMap['dependencies'];
-
-      if (deps == null) return new Future.value();
-
-      final List<Future> futures = [];
-
-      deps.forEach((packageName, packageSpec) {
-        futures.add(_fetchPackage(packageName, packageSpec, _packagesDir));
-      });
-
-      return Future.wait(futures).whenComplete(() {
-        final List<Future> subFutures = [];
-
-        deps.forEach((String packageName, String packageSpec) {
-          final String subSpecFname =
-              '$packageName/${bowerProperties.packageSpecFileName}';
-          _packagesDir.getFile(subSpecFname).then((chrome.FileEntry subSpecFile) {
-            if (subSpecFile != null) {
-              subFutures.add(getDependencies(subSpecFile));
-            }
-          });
-        });
-
-        // TODO: This ignores the first-level results in [futures].
-        return Future.wait(subFutures);
-      });
-    }).catchError((e, s) {
-      // Ignore: fetch as many dependencies as we can, regardless of errors.
-      return new Future.value();
-    });
-  }
-
-  Future _fetchPackage(
-      String packageName,
-      String packageSpec,
-      chrome.DirectoryEntry packagesDir) {
-    final Match match = _PACKAGE_SPEC_REGEXP.matchAsPrefix(packageSpec);
-    if (match == null) {
-      return new Future.error("Malformed Bower dependency: '$packageSpec'");
-    }
-    final String path = match.group(1);
-    // NOTE: This can be null, but _scmProvider.clone() can handle that.
-    final String branch = match.group(2);
-
-    return packagesDir.createDirectory(packageName).then((destDir) {
-      final String url = '$_GITHUB_ROOT_URL/$path';
-      return _scmProvider.clone(url, destDir, branchName: branch);
-    });
   }
 }
 
@@ -168,6 +97,7 @@ class _BowerBuilder extends PackageBuilder {
   PackageServiceProperties get properties => bowerProperties;
 
   String getPackageNameFromSpec(String spec) {
+    // TODO(ussuri): Handle parsing errors.
     final Map<String, dynamic> specMap = JSON.decode(spec);
     return specMap == null ? null : specMap['name'];
   }
---
+++ ide/app/lib/package_mgmt/bower_fetcher.dart
@@ -0,0 +1,142 @@
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/**
+ * Bower package fetcher.
+ */
+
+// TODO(ussuri): Add comments.
+
+library spark.package_mgmt.bower_fetcher;
+
+import 'dart:async';
+import 'dart:convert' show JSON;
+import 'dart:html' as html;
+
+import 'package:chrome/chrome_app.dart' as chrome;
+import 'package:logging/logging.dart';
+
+import '../scm.dart';
+
+Logger _logger = new Logger('spark.bower_fetcher');
+
+class BowerFetcher {
+  static final ScmProvider _git = getProviderType('git');
+
+  final chrome.DirectoryEntry _packagesDir;
+  final String _packageSpecFileName;
+
+  Map<String, _Package> _allDeps = {};
+
+  BowerFetcher(this._packagesDir, this._packageSpecFileName);
+
+  Future fetchDependencies(chrome.ChromeFileEntry specFile) {
+    return specFile.readText().then((spec) {
+      return _gatherAllDeps(spec).then((_) {
+        return _fetchAllDeps();
+      });
+    });
+  }
+
+  Future _gatherAllDeps(String spec) {
+    List<_Package> deps = _parseDepsFromSpec(spec);
+
+    List<Future> futures = [];
+
+    deps.forEach((_Package package) {
+      // TODO(ussuri): Handle conflicts: same names, different specs.
+      if (!_allDeps.containsKey(package.name)) {
+        _allDeps[package.name] = package;
+
+        final Future f = _readRemoteSpecFile(package).then((String depSpec) {
+          return _gatherAllDeps(depSpec);
+        }).catchError((e) {
+          // Remote spec file doesn't exist: this is a leaf package with no
+          // dependencies.
+          return new Future.value();
+        });
+
+        futures.add(f);
+      }
+    });
+
+    return Future.wait(futures);
+  }
+
+  Future _fetchAllDeps() {
+    List<Future> futures = [];
+    _allDeps.values.forEach((_Package package) =>
+        futures.add(_fetchPackage(package))
+    );
+    return Future.wait(futures);
+  }
+
+  List<_Package> _parseDepsFromSpec(String spec) {
+    // TODO(ussuri): Handle possible parsing errors.
+    final Map<String, dynamic> specMap = JSON.decode(spec);
+    Map<String, String> rawDeps = specMap['dependencies'];
+    if (rawDeps == null) rawDeps = {};
+
+    List<_Package> deps = [];
+    rawDeps.forEach((String name, String fullPath) =>
+      deps.add(new _Package(name, fullPath))
+    );
+
+    return deps;
+  }
+
+  Future<String> _readRemoteSpecFile(_Package package) {
+    final String url = package.getUrlForDownloading(_packageSpecFileName);
+    return html.HttpRequest.getString(url)
+        .catchError((e, s) =>
+            _logger.info('Error reading spec file for ${package.name}: $e'));
+  }
+
+  Future _fetchPackage(_Package package) {
+    return _packagesDir.createDirectory(package.name).then((destDir) {
+      // TODO(ussuri): Handle case where destination already exists.
+      return _git.clone(
+          package.getUrlForCloning(), destDir, branchName: package.branch)
+              .catchError((e, s) =>
+                  _logger.severe('Error cloning package ${package.name}: $e'));
+    });
+  }
+}
+
+class _Package {
+  /// E.g.: "Polymer/polymer-elements".
+  static const _PACKAGE_SPEC_PATH = '''[-\\w./]+''';
+  /// E.g.: "#master", "#1.2.3".
+  static const _PACKAGE_SPEC_BRANCH = '''[-\\w.]+''';
+  /// E.g.: "Polymer/polymer#master";
+  static final RegExp _PACKAGE_SPEC_REGEXP =
+      new RegExp('''^($_PACKAGE_SPEC_PATH)(?:#($_PACKAGE_SPEC_BRANCH))?\$''');
+
+  static const _GITHUB_ROOT_URL = 'https://github.com';
+  static const _GITHUB_USER_CONTENT_URL = 'https://raw.githubusercontent.com';
+
+  String name;
+  String fullPath;
+  String path;
+  String branch;
+
+  _Package(this.name, this.fullPath) {
+    final Match match = _PACKAGE_SPEC_REGEXP.matchAsPrefix(fullPath);
+    if (match == null) throw "Malformed Bower dependency: '$fullPath'";
+    path = match.group(1);
+    branch = match.group(2);
+    if (branch == null) {
+      branch = 'master';
+    }
+  }
+
+  bool operator==(_Package another) =>
+      path == another.path && branch == another.branch;
+
+  String getUrlForCloning() =>
+      '$_GITHUB_ROOT_URL/$path' + (path.endsWith('.git') ? '' : '.git');
+
+  String getUrlForDownloading(String relFilePath) =>
+      '$_GITHUB_USER_CONTENT_URL/$path/$branch/$relFilePath';
+}
