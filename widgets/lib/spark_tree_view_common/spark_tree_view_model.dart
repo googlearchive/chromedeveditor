777b9cd4a5f5f6e9bd5fac22180deeb04a27e3c1
Sergey Shevchenko <sergeygs@google.com>
2014-04-24 13:32:06 -0700

    Small adjustments

--- widgets/lib/spark_tree_view_common/spark_tree_view_model.dart
+++ widgets/lib/spark_tree_view_common/spark_tree_view_model.dart
@@ -9,21 +9,21 @@ import '../common/spark_widget.dart';
 abstract class SparkTreeViewModel {
   Iterable<String> getChildrenNames(String nodePath);
 
-  String getInnerHtml(String nodePath);
-
-  // TODO(ussuri): All these could be replaced by custom events fired from
-  // [SparkTreeNodeView].
-
-  void select(String nodePath);
+  String getNodeInnerHtml(String nodePath);
 
   /// Should return a list of [SparkMenuItem]s and [SparkMenuSeparator]s.
   List<SparkWidget> getContextMenu(String nodePath);
 
+  // TODO(ussuri): All these could be replaced by custom events fired from
+  // [SparkTreeNodeView].
+
+  void onSelect(String nodePath);
+
   /// The returned bool indicates whether the tree-node has changed as a result of
   /// the action, and thus needs to be redrawn.
-  bool applyContextMenuAction(String nodePath, String actionId);
+  bool onApplyContextMenuAction(String nodePath, String actionId);
 
-  void startDragging(List<String> nodePaths);
+  void onDragStart(List<String> nodePaths);
 
-  void drop(List<String> nodePaths, String targetNodePath);
+  void onDrop(List<String> nodePaths, String targetNodePath);
 }
