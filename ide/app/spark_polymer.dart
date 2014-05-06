943d30e7ec1f77c6837ea298188e849fcbc5e9f0
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-05-06 12:44:27 -0700

    Improved save feedback

--- ide/app/spark_polymer.dart
+++ ide/app/spark_polymer.dart
@@ -178,6 +178,11 @@ class SparkPolymer extends Spark {
     eventBus.onEvent(BusEventType.EDITOR_MANAGER__FILES_SAVED).listen((_) {
       statusComponent.temporaryMessage = 'All changes saved';
     });
+    // When nothing had to be saved, show the same feedback to make the user
+    // happy.
+    eventBus.onEvent(BusEventType.EDITOR_MANAGER__NO_MODIFICATIONS).listen((_) {
+      statusComponent.temporaryMessage = 'All changes saved';
+    });
 
     // Listen for job manager events.
     jobManager.onChange.listen((JobManagerEvent event) {
