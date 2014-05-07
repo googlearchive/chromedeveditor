1e683cffe952d474b9ee0af93dfda56721caa9cb
Sergey Shevchenko <sergeygs@google.com>
2014-05-07 16:10:44 -0700

    Adjustments

--- widgets/lib/spark_button/spark_button.dart
+++ widgets/lib/spark_button/spark_button.dart
@@ -11,15 +11,14 @@ import '../common/spark_widget.dart';
 @CustomTag('spark-button')
 class SparkButton extends SparkWidget {
   @published bool primary = false;
-
-  // TODO: changing this field does not cause the btnClasses to be re-calculated
+  // TODO(ussuri): with `@published enabled`, changing the field from outside
+  // didn't immediately trigger enabledChanged(). Investigate why.
   bool _enabled = true;
   @published bool get enabled => _enabled;
-  set enabled(bool value) {
+  @published set enabled(bool value) {
     _enabled = value;
     _setClasses();
   }
-
   @published bool large = false;
   @published bool small = false;
   @published bool noPadding = false;
@@ -30,8 +29,6 @@ class SparkButton extends SparkWidget {
   void enteredView() {
     _setClasses();
   }
-
-  void enabledChanged() => _setClasses();
 
   void _setClasses() {
     $['button'].classes
