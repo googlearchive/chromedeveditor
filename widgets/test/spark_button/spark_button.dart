473b32f85da1308eaaafc9e20a51bf0ed8f2f18c
Sergey Shevchenko <sergeygs@google.com>
2014-05-12 13:28:45 -0700

    Upgrade spark-button test to new Polymer and new spark-button

--- widgets/test/spark_button/spark_button.dart
+++ widgets/test/spark_button/spark_button.dart
@@ -7,77 +7,73 @@ library test.spark_button;
 import 'dart:async' as async;
 import 'dart:html' as dom;
 
-import 'package:polymer/polymer.dart';
+import 'package:polymer/polymer.dart' as polymer;
 import 'package:unittest/unittest.dart';
 import 'package:unittest/html_enhanced_config.dart';
 
 import 'package:spark_widgets/spark_button/spark_button.dart';
-import 'package:spark_widgets/common/spark_widget.dart';
 
+@polymer.initMethod
 void main() {
   useHtmlEnhancedConfiguration();
 
-  initPolymer().run(() {
-    Polymer.onReady.then((e) {
+  polymer.Polymer.onReady.then((e) {
 
-      group('spark-button', () {
-        test('default click', () {
-          var btn = (dom.document.querySelector('#default') as SparkButton);
-          expect(btn, isNotNull);
+    group('spark-button', () {
+      test('default click', () {
+        var btn = (dom.document.querySelector('#default') as SparkButton);
+        expect(btn, isNotNull);
 
-          async.StreamSubscription clickSubsrc;
+        async.StreamSubscription clickSubsrc;
 
-          var clicked = expectAsync((dom.Event event) {
-            expect(event.target, equals(btn));
-            clickSubsrc.cancel();
-          });
-          clickSubsrc = btn.onClick.listen(clicked);
-          btn.click();
+        var clicked = expectAsync((dom.Event event) {
+          expect(event.target, equals(btn));
+          clickSubsrc.cancel();
         });
+        clickSubsrc = btn.onClick.listen(clicked);
+        btn.click();
+      });
 
-        test('attributes', () {
-          var btn = (dom.document.querySelector('#default') as SparkButton);
-          expect(btn, isNotNull);
+      test('attributes', () {
+        var btn = (dom.document.querySelector('#default') as SparkButton);
+        expect(btn, isNotNull);
 
-          btn.attributes['primary'] = 'true';
-          btn.attributes['active'] = 'true';
-          btn.attributes['large'] = 'true';
-          btn.attributes['small'] = 'false';
-          btn.attributes['noPadding'] = 'true';
+        btn.attributes['primary'] = 'true';
+        btn.attributes['active'] = 'true';
+        btn.attributes['large'] = 'true';
+        btn.attributes['small'] = 'false';
+        btn.attributes['noPadding'] = 'true';
 
-          var done = expectAsync((){});
+        var done = expectAsync((){});
 
-          new async.Future.delayed(new Duration(milliseconds: 50),() {
-            expect(btn.btnClasses[SparkButton.CSS_PRIMARY], isTrue);
-            expect(btn.btnClasses[SparkButton.CSS_DEFAULT], isFalse);
-            expect(btn.btnClasses[SparkWidget.CSS_ENABLED], isTrue);
-            expect(btn.btnClasses[SparkWidget.CSS_DISABLED], isFalse);
-            expect(btn.btnClasses[SparkButton.CSS_LARGE], isTrue);
-            expect(btn.btnClasses[SparkButton.CSS_SMALL], isFalse);
-            expect(btn.noPadding, isTrue);
-            expect(btn.shadowRoot.querySelector('#button').getComputedStyle().padding, equals('0px'));
-            expect(btn.attributes['action-id'], isNull);
+        new async.Future.delayed(new Duration(milliseconds: 50),() {
+          expect(btn.classes.contains('btn-primary'), isTrue);
+          expect(btn.classes.contains('btn-default'), isFalse);
+          expect(btn.classes.contains('enabled'), isTrue);
+          expect(btn.classes.contains('disabled'), isFalse);
+          expect(btn.classes.contains('btn-lg'), isTrue);
+          expect(btn.classes.contains('btn-sm'), isFalse);
+          expect(btn.noPadding, isTrue);
+          expect(btn.getShadowDomElement('#button').getComputedStyle().padding,
+                 equals('0px'));
 
-            btn.attributes['primary'] = 'false';
-            btn.attributes['active'] = 'false';
-            btn.attributes['large'] = 'false';
-            btn.attributes['small'] = 'true';
-            btn.attributes['noPadding'] = 'false';
-            btn.attributes['action-id'] = 'someAction';
+          btn.attributes['primary'] = 'false';
+          btn.attributes['active'] = 'false';
+          btn.attributes['large'] = 'false';
+          btn.attributes['small'] = 'true';
+          btn.attributes['noPadding'] = 'false';
 
-
-            new async.Future.delayed(new Duration(milliseconds: 550), () {
-              expect(btn.btnClasses[SparkButton.CSS_PRIMARY], isFalse);
-              expect(btn.btnClasses[SparkButton.CSS_DEFAULT], isTrue);
-              expect(btn.btnClasses[SparkWidget.CSS_ENABLED], isFalse);
-              expect(btn.btnClasses[SparkWidget.CSS_DISABLED], isTrue);
-              expect(btn.btnClasses[SparkButton.CSS_LARGE], isFalse);
-              expect(btn.btnClasses[SparkButton.CSS_SMALL], isTrue);
-              expect(btn.noPadding, isFalse);
-              expect(btn.shadowRoot.querySelector('#button').getComputedStyle().padding, isNot(equals('10px')));
-              expect(btn.attributes['action-id'], equals('someAction'));
-              done();
-            });
+          new async.Future.delayed(new Duration(milliseconds: 550), () {
+            expect(btn.classes.contains('btn-primary'), isFalse);
+            expect(btn.classes.contains('btn-default'), isTrue);
+            expect(btn.classes.contains('enabled'), isFalse);
+            expect(btn.classes.contains('disabled'), isTrue);
+            expect(btn.classes.contains('btn-lg'), isFalse);
+            expect(btn.classes.contains('btn-sm'), isTrue);
+            expect(btn.noPadding, isFalse);
+            expect(btn.getShadowDomElement('#button').getComputedStyle().padding,
+                   isNot(equals('10px')));
+            done();
           });
         });
       });
