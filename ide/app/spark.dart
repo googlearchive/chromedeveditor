8a7cc0ae3ea34898e0c5d2102f86064c61072392
Eric Arnold <ericarnold@google.com>
2014-05-23 13:34:55 -0700

    review fixes

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -1772,10 +1772,10 @@ class GotoDeclarationAction extends SparkAction {
     if (editor is TextEditor) {
       editor.navigateToDeclaration(new Duration(milliseconds: 500)).then(
           (Declaration declaration) {
-            if (declaration == null) spark.showSuccessMessage(NOT_FOUND_ERROR);
-          }).catchError((TimeoutException e) {
-            spark.showSuccessMessage(TIMEOUT_ERROR);
-          });
+        if (declaration == null) spark.showSuccessMessage(NOT_FOUND_ERROR);
+      }).catchError((TimeoutException e) {
+        spark.showSuccessMessage(TIMEOUT_ERROR);
+      });
     }
   }
 }
