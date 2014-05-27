258a188b6505fe926c2cc613dd17a15d2ce1600e
Gaurav Agarwal <grv@chromium.org>
2014-05-27 00:02:35 -0700

    cleanup

--- ide/app/lib/launch.dart
+++ ide/app/lib/launch.dart
@@ -234,7 +234,6 @@ class ChromeAppLaunchDelegate extends LaunchDelegate {
 
   Future run(Resource resource) {
     Container launchContainer = getAppContainerFor(resource);
-    window.console.log(launchContainer.entry);
     return updateManifest(launchContainer.entry).then((_) {
 
     return developerPrivate.loadDirectory(launchContainer.entry).then((String appId) {
