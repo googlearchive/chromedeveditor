dcbb9144a94dcc2f36df17b8a45ca9939aa8d4de
Sergey Shevchenko <sergeygs@google.com>
2014-05-28 03:32:21 -0700

    Simplified template code a little

--- ide/app/lib/templates/addons/bower_deps/template.dart
+++ ide/app/lib/templates/addons/bower_deps/template.dart
@@ -2,18 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library spark.templates.bower_deps;
+part of spark.templates;
 
-import '../../templates.dart';
-
-class Template extends ProjectTemplate {
-  Template(String id, List<TemplateVar> globalVars, List<TemplateVar> localVars)
-      : super.internal(id, globalVars, localVars);
-
-  @override
-  List<TemplateVar> computeDerivedVars(
-      List<TemplateVar>globalVars, List<TemplateVar>localVars) {
-    final List<TemplateVar> derivedVars = [];
-    return derivedVars;
-  }
+class BowerDepsTemplate extends ProjectTemplate {
+  BowerDepsTemplate(
+      String id, List<TemplateVar> globalVars, List<TemplateVar> localVars)
+      : super._(id, globalVars, localVars);
 }
