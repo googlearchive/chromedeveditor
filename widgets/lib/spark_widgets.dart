23e638e0a980ce64a6f5e974c308299d450cbb76
Sergey Shevchenko <sergeygs@google.com>
2014-05-28 22:47:05 -0700

    Initial version of spark-dialog
    More-or-less works but is badly stiled (turned off Bootstrap)

--- widgets/lib/spark_widgets.dart
+++ widgets/lib/spark_widgets.dart
@@ -6,6 +6,9 @@ export "common/spark_widget.dart";
 import "spark_button/spark_button.dart";
 export "spark_button/spark_button.dart";
 
+import "spark_dialog/spark_dialog.dart";
+export "spark_dialog/spark_dialog.dart";
+
 import "spark_icon/spark_icon.dart";
 export "spark_icon/spark_icon.dart";
 
