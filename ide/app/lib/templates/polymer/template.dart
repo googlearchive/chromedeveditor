39e1efca9c7e8b90c20a8f75582719371aaaae05
Sergey Shevchenko <sergeygs@google.com>
2014-05-28 17:27:34 -0700

    Comment

--- ide/app/lib/templates/polymer/template.dart
+++ ide/app/lib/templates/polymer/template.dart
@@ -10,6 +10,8 @@ class PolymerTemplate extends ProjectTemplate {
       : super._(id, globalVars, localVars) {
     final String projName = _vars['projectName'].value;
 
+    // Convert all non-numalpha chars to dashes, and make sure there is at
+    // least one dash in the tag name: this is required for a custom element.
     String tagName = projName.toLowerCase().replaceAll(new RegExp(r'\W|_'), '-');
     if (!tagName.contains('-')) {
       tagName = 'x-$tagName';
