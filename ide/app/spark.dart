582738c82b0d943cc71abce5a74201d55ff3812a
Sergey Shevchenko <sergeygs@google.com>
2014-06-02 22:03:34 -0700

    Fix bogus "PLACEHOLDER" title in error and confirmation dialogs

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -596,7 +596,8 @@ abstract class Spark
       _errorDialog.getShadowDomElement("#closingX").onClick.listen(_hideBackdropOnClick);
     }
 
-    _errorDialog.dialog.title = title;
+    // TODO(ussuri): Replace with ...title = title once BUG #2252 is resolved.
+    _errorDialog.dialog.setAttr('title', true, title);
 
     Element container = _errorDialog.getElement('#errorMessage');
     container.children.clear();
@@ -670,7 +671,8 @@ abstract class Spark
       });
     }
 
-    _okCancelDialog.dialog.title = title;
+    // TODO(ussuri): Replace with ...title = title once BUG #2252 is resolved.
+    _okCancelDialog.dialog.setAttr('title', true, title);
 
     Element container = _okCancelDialog.getElement('#okCancelMessage');
     container.children.clear();
