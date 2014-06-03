74a2a39cc980f68a784c22b6aca10d832d58b9c4
Sergey Shevchenko <sergeygs@google.com>
2014-06-02 23:59:27 -0700

    Changed "Import Folder" -> "Add Folder to Workspace" per review discussion

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -1995,7 +1995,7 @@ class NewProjectAction extends SparkActionWithDialog {
 }
 
 class FolderOpenAction extends SparkAction {
-  FolderOpenAction(Spark spark) : super(spark, "folder-open", "Import Folder…");
+  FolderOpenAction(Spark spark) : super(spark, "folder-open", "Add Folder to Workspace…");
 
   void _invoke([Object context]) {
     spark.openFolder();
@@ -3376,7 +3376,7 @@ class ImportFileAction extends SparkAction implements ContextAction {
 }
 
 class ImportFolderAction extends SparkAction implements ContextAction {
-  ImportFolderAction(Spark spark) : super(spark, "folder-import", "Import Folder…");
+  ImportFolderAction(Spark spark) : super(spark, "folder-import", "Add Folder to Workspace…");
 
   void _invoke([List<ws.Resource> resources]) {
     spark.importFolder(resources);
