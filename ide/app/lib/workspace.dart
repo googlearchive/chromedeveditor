0c0464c4f0f5918f3b9e852adef7d0d8aed5f43a
Gaurav Agarwal <grv@chromium.org>
2014-06-06 02:26:25 -0700

    fixed typo.

--- ide/app/lib/workspace.dart
+++ ide/app/lib/workspace.dart
@@ -261,7 +261,7 @@ class Workspace extends Container {
           _logger.info('Workspace restore took ${stopwatch.elapsedMilliseconds}ms.');
           resumeResourceEvents();
           // Disable syncfs projects.
-          // TODO(grv): Re-anable it once the syncfs api is more stable.
+          // TODO(grv): Re-enable it once the syncfs api is more stable.
           //_restoreSyncFs();
         }).then((_) => _whenAvailable.complete(this));
       } catch (e) {
