272112b1d3751fefede5d721c09512f24e341e73
Devon Carew <devoncarew@gmail.com>
2014-06-10 13:03:59 -0700

    last rename

--- ide/app/lib/mobile/adb.dart
+++ ide/app/lib/mobile/adb.dart
@@ -351,7 +351,7 @@ class AndroidDevice {
     return chrome.usb.claimInterface(adbConnectionHandle,
         adbInterface.interfaceNumber).catchError((e) {
       throw 'Could not open an ADB connection: "${e}".\n Please check whether the'
-          'Chrome App Harness application is running on the Android device. \n'
+          'App Dev Tool application is running on the Android device. \n'
           'Additionally, DevTools may not have released the USB connection. To '
           'check this go to chrome://inspect, Devices, uncheck \'Discover USB '
           'devices\', then disconnect and re-connect your phone from USB.';
@@ -543,8 +543,7 @@ class AndroidDevice {
         return msg.arg0;
       } else {
         return new Future.error('Expected an OKAY but got: ${msg.toString()}.\n'
-            'Please check that you are running the Chrome App Harness on your '
-            'mobile device.');
+            'Please check that you are running the App Dev Tool on your mobile device.');
       }
     });
   }
@@ -565,9 +564,8 @@ class AndroidDevice {
       // If we caught an error here, we probably got a CLSE instead.
       // This means the remote end isn't listening to our port.
       _logger.severe('Error sending adb message', e);
-      return new Future.error('Connection on port $port refused.\n'
-          'Please check whether the Chrome App Harness is running on your '
-          'mobile device.');
+      return new Future.error('Connection on port $port refused.\nPlease check '
+          'whether the App Dev Tool is running on your mobile device.');
     }).then((_) { return awaitOkay(); }).then((remoteID_) {
       remoteID = remoteID_;
 
--- ide/app/lib/mobile/adb_client.dart
+++ ide/app/lib/mobile/adb_client.dart
@@ -36,9 +36,9 @@ class AdbApplication {
   static AdbApplication CONTENT_SHELL = new AdbApplication(
       'org.chromium.content_shell_apk', '.ContentShellActivity');
 
-  /// Chrome Apps Developer Tool (ADT).
+  /// Chrome App Developer Tool for Mobile (App Dev Tool).
   static AdbApplication CHROME_ADT = new AdbApplication(
-      'org.chromium.appharness', 'org.chromium.appharness.ChromeAppHarness');
+      'org.chromium.appdevtool', 'org.chromium.appdevtool.ChromeAppHarness');
 
   final String packageName;
   final String mainActivity;
