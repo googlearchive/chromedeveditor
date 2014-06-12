3d717307cb7321ee83c5534bb3c974211c47b513
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-06-12 14:52:58 -0700

    Fied radio buttons alignments

--- ide/app/spark_polymer.dart
+++ ide/app/spark_polymer.dart
@@ -165,6 +165,9 @@ class SparkPolymer extends Spark {
   void uiReady() {
     assert(_ui == null);
     _ui = document.querySelector('#topUi');
+    if (PlatformInfo.isMac) {
+      _ui.classes.add('mac');
+    }
   }
 
   @override
