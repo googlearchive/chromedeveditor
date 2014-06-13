f68c0dab83be622d594954dac0170508c7ba9b71
Adam Singer <financeCoding@gmail.com>
2014-06-12 17:30:19 -0700

    remove other direct references to ace

--- ide/app/test/ace_test.dart
+++ ide/app/test/ace_test.dart
@@ -7,7 +7,6 @@ library spark.ace_test;
 import 'dart:async';
 import 'dart:html';
 
-import 'package:ace/ace.dart' as ace;
 import 'package:unittest/unittest.dart';
 
 import '../lib/ace.dart';
@@ -93,7 +92,7 @@ class MockAceEditor implements TextEditor {
 
   Future save() => new Future.value();
 
-  void setSession(ace.EditSession value) { }
+  void setSession(EditSession value) { }
 
   void fileContentsChanged() { }
 
