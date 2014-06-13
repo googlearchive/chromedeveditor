28802b5ba4a4715755e7f9c1e062a569bad6408d
Gaurav Agarwal <grv@chromium.org>
2014-06-13 03:08:30 -0700

    Make GitException string constants.

--- ide/app/lib/git/exception.dart
+++ ide/app/lib/git/exception.dart
@@ -28,40 +28,40 @@ class GitException implements Exception {
  */
 class GitErrorConstants {
 
-  static final String GIT_HTTP_NOT_FOUND_ERROR = "git.http_not_found_error";
-  static final String GIT_HTTP_FORBIDDEN_ERROR = "git.http_forbidden_error";
-  static final String GIT_HTTP_ERROR = "git.http_error";
-  static final String GIT_AUTH_REQUIRED = "git.auth_required";
-  static final String GIT_AUTH_ERROR = "git.auth_error";
+  static const String GIT_HTTP_NOT_FOUND_ERROR = "git.http_not_found_error";
+  static const String GIT_HTTP_FORBIDDEN_ERROR = "git.http_forbidden_error";
+  static const String GIT_HTTP_ERROR = "git.http_error";
+  static const String GIT_AUTH_REQUIRED = "git.auth_required";
+  static const String GIT_AUTH_ERROR = "git.auth_error";
 
-  static final String GIT_CLONE_DIR_NOT_EMPTY = "git.clone_dir_not_empty";
-  static final String GIT_CLONE_DIR_IN_USE = "git.clone_dir_in_use";
-  static final String GIT_CLONE_DIR_NOT_INITIALIZED
+  static const String GIT_CLONE_DIR_NOT_EMPTY = "git.clone_dir_not_empty";
+  static const String GIT_CLONE_DIR_IN_USE = "git.clone_dir_in_use";
+  static const String GIT_CLONE_DIR_NOT_INITIALIZED
       = "git.clone_dir_not_initialized";
-  static final String GIT_CLONE_CANCEL = "git.clone_cancel";
+  static const String GIT_CLONE_CANCEL = "git.clone_cancel";
 
-  static final String GIT_BRANCH_UP_TO_DATE = "git.branch_up_to_date";
-  static final String GIT_BRANCH_NOT_FOUND = "git.branch_not_found";
-  static final String GIT_REMOTE_BRANCH_NOT_FOUND
+  static const String GIT_BRANCH_UP_TO_DATE = "git.branch_up_to_date";
+  static const String GIT_BRANCH_NOT_FOUND = "git.branch_not_found";
+  static const String GIT_REMOTE_BRANCH_NOT_FOUND
       = "git.remote_branch_not_found";
-  static final String GIT_BRANCH_EXISTS = "git.branch_exists";
-  static final String GIT_INVALID_BRANCH_NAME = "git.invalid_branch_name";
+  static const String GIT_BRANCH_EXISTS = "git.branch_exists";
+  static const String GIT_INVALID_BRANCH_NAME = "git.invalid_branch_name";
 
-  static final String GIT_PUSH_NO_REMOTE = "git.push_no_remote";
-  static final String GIT_PUSH_NO_COMMITS = "git.push_no_commits";
-  static final String GIT_PUSH_NON_FAST_FORWARD = "git.push_non_fast_forward";
+  static const String GIT_PUSH_NO_REMOTE = "git.push_no_remote";
+  static const String GIT_PUSH_NO_COMMITS = "git.push_no_commits";
+  static const String GIT_PUSH_NON_FAST_FORWARD = "git.push_non_fast_forward";
 
-  static final String GIT_OBJECT_STORE_CORRUPTED = "git.object_store_corrupted";
+  static const String GIT_OBJECT_STORE_CORRUPTED = "git.object_store_corrupted";
 
-  static final String GIT_COMMIT_NO_CHANGES = "git.commit_no_changes";
+  static const String GIT_COMMIT_NO_CHANGES = "git.commit_no_changes";
 
-  static final String GIT_FETCH_UP_TO_DATE = "git.fetch_up_to_date";
+  static const String GIT_FETCH_UP_TO_DATE = "git.fetch_up_to_date";
 
-  static final String GIT_MERGE_ERROR = "git.merge_error";
+  static const String GIT_MERGE_ERROR = "git.merge_error";
 
-  static final String GIT_INVALID_REPO_URL = "git.invalid_repo_url";
-  static final String GIT_WORKING_TREE_NOT_CLEAN = "git.working_tree_not_clean";
-  static final String GIT_FILE_STATUS_TYPE_UNKNOWN = "git.file_status_type_unknown";
-  static final String GIT_SUBMODULES_NOT_YET_SUPPORTED
+  static const String GIT_INVALID_REPO_URL = "git.invalid_repo_url";
+  static const String GIT_WORKING_TREE_NOT_CLEAN = "git.working_tree_not_clean";
+  static const String GIT_FILE_STATUS_TYPE_UNKNOWN = "git.file_status_type_unknown";
+  static const String GIT_SUBMODULES_NOT_YET_SUPPORTED
       = "git.submodules_not_yet_supported";
 }
