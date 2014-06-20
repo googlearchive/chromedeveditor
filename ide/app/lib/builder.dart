5eb5fd50502e5bd23ae541cd1f307a569d791c28
Sergey Shevchenko <sergeygs@google.com>
2014-06-19 18:09:13 -0700

    Fix errors

--- ide/app/lib/builder.dart
+++ ide/app/lib/builder.dart
@@ -13,9 +13,10 @@ import 'dart:async';
 import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 
-import 'workspace.dart';
+import '../spark_flags.dart';
 import 'jobs.dart';
 import 'package_mgmt/package_utils.dart';
+import 'workspace.dart';
 
 final Logger _logger = new Logger('spark.builder');
 final NumberFormat _nf = new NumberFormat.decimalPattern();
@@ -82,7 +83,7 @@ class BuilderManager {
     if (event.isEmpty) return;
 
     _logger.info('starting build' +
-      SparkFlags.developerMode ? ' for ${event.changes}' : '');
+        (SparkFlags.developerMode ? ' for ${event.changes}' : ''));
 
     Stopwatch timer = new Stopwatch()..start();
 
