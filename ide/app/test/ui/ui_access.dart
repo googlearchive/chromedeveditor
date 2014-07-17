807a1cbf1f44309a18e202d875d65d08adbd3ca3
Eric Arnold <ericarnold@google.com>
2014-07-17 01:59:46 -0700

    Cleanup

--- ide/app/test/ui/ui_access.dart
+++ ide/app/test/ui/ui_access.dart
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-library spark.workspace_test;
+library spark.ui_access;
 
 import 'dart:async';
 import 'dart:html';
