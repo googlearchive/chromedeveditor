964cb2acc144af4997243d0bf45d2593ebc445d0
Konstantin Scheglov <Konstantin.Scheglov@gmail.com>
2014-08-25 00:13:53 -0700

    try

--- ide/app/lib/ace.dart
+++ ide/app/lib/ace.dart
@@ -506,8 +506,8 @@ class AceManager {
     //_aceEditor.setOption('enableSnippets', true);
 
     ace.require('ace/ext/linking');
-    _aceEditor.setOptions({'enableMultiselect' : false,
-                           'enableLinking' : true});
+    _aceEditor.setOptions({'enableMultiselect': false,
+                           'enableLinking': true});
 
     _aceEditor.onLinkHover.listen((ace.LinkEvent event) {
       if (!DartEditor.isDartFile(currentFile)) {
@@ -957,8 +957,6 @@ class ThemeManager {
       'katzenmilch',
       'kuroir', 
       'solarized_light',
-      'tomorrow',
-      'vibrant_ink',
       'xcode',
   ];
   static final DARK_THEMES = [
@@ -982,6 +980,7 @@ class ThemeManager {
       'tomorrow_night_bright',
       'tomorrow_night_eighties',
       'twilight',
+      'vibrant_ink',
   ];
 
   ace.Editor _aceEditor;
