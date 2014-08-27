2da1c5cccd2af2b30bb0dbb584c7983cf9284d7f
Gaurav Agarwal <grv@grv-macbookair2.roam.corp.google.com>
2014-08-27 14:26:12 -0700

    Throw original exception when clone fails.

--- ide/app/lib/git/commands/clone.dart
+++ ide/app/lib/git/commands/clone.dart
@@ -66,8 +66,7 @@ class Clone {
     return fetcher.isValidRepoUrl().then((isValid) {
       if (isValid) {
         return startClone(fetcher).catchError((e) {
-          _cleanup();
-          throw e;
+          return _cleanup().then((_) => throw e, onError: (_) => throw e);
         });
       } else if (!_options.repoUrl.endsWith('.git')) {
         _options.repoUrl += '.git';
