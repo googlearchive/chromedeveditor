c3f00d5318abdb9e9e2b8a91b7e3a428fd6eaebf
Devon Carew <devoncarew@gmail.com>
2014-08-31 13:41:01 -0700

    create cde_workbench

---
+++ cde-workbench.dart/lib/cde_workbench.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer cde-workbench element.
+ */
+@CustomTag('cde-workbench')
+class CdeWorkbench extends PolymerElement {
+
+  CdeWorkbench.created() : super.created();
+}
