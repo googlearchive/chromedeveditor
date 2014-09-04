5917eab005a39f377706b5c83d146821720f3827
Devon Carew <devoncarew@gmail.com>
2014-09-04 16:14:06 -0700

    rename app/ to web/; refactoring ensues

--- ide/tool/test_runner.dart
+++ ide/tool/test_runner.dart
@@ -5,18 +5,5 @@
 import 'package:chrome_testing/testing_cli.dart' as testing;
 
 void main([List<String> args = const []]) {
-  // Tweak the params to maintain compatibility wth the previous CLI args.
-  if (args.length == 1) {
-    args = new List.from(args);
-
-    if (args[0] == '--dartium') {
-      args.add('--appPath');
-      args.add('app');
-    } else if (args[0] == '--chrome') {
-      args.add('--appPath');
-      args.add('build/deploy-out/web');
-    }
-  }
-
   testing.performTesting(args);
 }
