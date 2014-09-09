238c9ec405dfcf6996254ca555c714336f6cbeb2
keertip <keertip@google.com>
2014-09-09 09:55:32 -0700

    modify script for use with chrome and dartium

--- cde-messaging.dart/hostpy/uninstall_host.sh
+++ cde-messaging.dart/hostpy/uninstall_host.sh
@@ -8,18 +8,22 @@ set -e
 if [ $(uname -s) == 'Darwin' ]; then
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR_CHROMIUM="/Library/Chromium/NativeMessagingHosts"
   else
-    TARGET_DIR=\
-        "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR_CHROMIUM="$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
   fi
 else
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+    TARGET_DIR_CHROMIUM="/etc/opt/chromium/native-messaging-hosts"
   else
     TARGET_DIR='$HOME/.config/google-chrome/NativeMessagingHosts'
+    TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
   fi
 fi
 
 HOST_NAME=com.google.chrome.example.echo
-rm $TARGET_DIR/com.google.chrome.example.echo.json
+rm "$TARGET_DIR/com.google.chrome.example.echo.json"
+rm "$TARGET_DIR_CHROMIUM/com.google.chrome.example.echo.json"
 echo Native messaging host $HOST_NAME has been uninstalled.
