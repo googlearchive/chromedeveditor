ff51762c89d037d3198fb169556ca954aa97b243
keertip <keertip@google.com>
2014-09-09 11:34:35 -0700

    fix indentation

--- cde-messaging.dart/host/install_host.sh
+++ cde-messaging.dart/host/install_host.sh
@@ -7,21 +7,21 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 if [ $(uname -s) == 'Darwin' ]; then
-if [ "$(whoami)" == "root" ]; then
-TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
-TARGET_DIR_CHROMIUM="/Library/Chromium/NativeMessagingHosts"
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR_CHROMIUM="/Library/Chromium/NativeMessagingHosts"
+  else
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR_CHROMIUM="$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
+  fi
 else
-TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
-TARGET_DIR_CHROMIUM="$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
-fi
-else
-if [ "$(whoami)" == "root" ]; then
-TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
-TARGET_DIR_CHROMIUM="/etc/opt/chromium/native-messaging-hosts"
-else
-TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
-TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
-fi
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+    TARGET_DIR_CHROMIUM="/etc/opt/chromium/native-messaging-hosts"
+  else
+    TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+    TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
+  fi
 fi
 
 HOST_NAME=com.google.chrome.example.dart
