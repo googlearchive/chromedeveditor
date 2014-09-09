da659fc1b542e7a34cc6e65657d9a4dd151d7f69
keertip <keertip@google.com>
2014-09-09 08:20:15 -0700

    fix nits

--- cde-messaging.dart/dartApp/web/main.dart
+++ cde-messaging.dart/dartApp/web/main.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 import 'dart:html';
 import 'dart:js';
 
@@ -10,7 +11,7 @@ void main() {
   updateUiState();
 }
 
-JsObject port = null;
+JsObject port;
 
  void _appendMessage(text) {
    querySelector("#response").text += text;
@@ -23,7 +24,7 @@ void updateUiState() {
 }
 
 void sendNativeMessage(MouseEvent event) {
- InputElement element = querySelector("#input_text");
+  InputElement element = querySelector("#input_text");
   String message = '{"action":${element.value}}';
   port.callMethod('postMessage', [message]);
   _appendMessage('Sent message:  $message');
