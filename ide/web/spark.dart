b9e55b4a87df5f25995830d440f8ebd577717056
Sergey Shevchenko <sergeygs@google.com>
2014-09-12 14:39:08 -0700

    Revert "Restrict "Properties" context menu item only to a single selected resource"
    This reverts commit e02824cb53249b47ac33e606fe8200891fd60b0f.

--- ide/web/spark.dart
+++ ide/web/spark.dart
@@ -2519,8 +2519,7 @@ class PropertiesAction extends SparkActionWithDialog implements ContextAction {
 
   String get category => 'properties';
 
-  // "Properties" make sense only for a single resource.
-  bool appliesTo(List context) => context.length == 1;
+  bool appliesTo(context) => true;
 }
 
 /* Git operations */
