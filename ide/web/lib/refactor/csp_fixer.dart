e2ccbe96e710675d82cc6f0f06222907b8e962b3
Sergey Shevchenko <sergeygs@google.com>
2014-09-12 12:26:10 -0700

    A slightly better version of the prev commit

--- ide/web/lib/refactor/csp_fixer.dart
+++ ide/web/lib/refactor/csp_fixer.dart
@@ -40,8 +40,8 @@ class CspFixer {
   static bool mightProcess(List resources) {
     // TODO(ussuri): Expand onto
     return resources.any((r) {
-      return (r is ws.Folder && mightProcess(r.getChildren())) ||
-             (r is ws.File && _HTML_FNAME_RE.matchAsPrefix(r.name) != null);
+      return (r is ws.File && _HTML_FNAME_RE.matchAsPrefix(r.name) != null) ||
+             (r is ws.Container && mightProcess(r.getChildren()));
     });
   }
 
