d0a76caf4ec85ec07e9d67ecf6a2dfc1f2b20843
Sergey Shevchenko <sergeygs@google.com>
2014-09-19 04:02:07 -0700

    Wrap Polymer Designer into a Dart polymer element

---
+++ cde-polymer-designer.dart/lib/cde_polymer_designer.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2013, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+library cde_polymer_designer;
+
+import 'dart:async';
+import 'dart:html';
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('cde-polymer-designer')
+class CdePolymerDesigner extends PolymerElement {
+  CdePolymerDesigner.created() : super.created();
+
+  @override
+  void attached() {
+    super.attached();
+  }
+}
