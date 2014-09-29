1a7f2547375814f39ca3c1d4d3ff7d56ef6d4bad
Alexandru Albu <albu@google.com>
2014-09-29 08:54:57 -0700

    changed to send the zip file

--- ide/web/lib/mobile/deploy.dart
+++ ide/web/lib/mobile/deploy.dart
@@ -377,11 +377,12 @@ abstract class AbstractDeployer {
       ad = archivedData;
       httpRequest = _makeBuildRequest(_getTarget(), archivedData);
       //TODO(albualexandru): make the request and save the file
-      //return _setTimeout(_pushRequestToDevice(httpRequest, BUILD_REQUEST_TIMEOUT));
-      return new Future.value("bla");
-//    }).then((msg) {
-//          return _expectHttpOkResponse(msg);
+      return _setTimeout(_pushRequestToDevice(httpRequest, BUILD_REQUEST_TIMEOUT));
+      //return new Future.value("bla");
+    }).then((msg) {
+          return _expectHttpOkResponse(msg);
     }).then((String response) {
+        print(response);
       //need to prompt for save as to save the APK
       chrome.ChooseEntryOptions options = new chrome.ChooseEntryOptions(
               type: chrome.ChooseEntryType.SAVE_FILE);
