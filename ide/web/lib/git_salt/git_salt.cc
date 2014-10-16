42ddce6547705a8c0946262525f3fae17806ea11
Gaurav Agarwal <grv@grv-macbookair2.roam.corp.google.com>
2014-10-15 17:15:20 -0700

    typo.

--- ide/web/lib/git_salt/git_salt.cc
+++ ide/web/lib/git_salt/git_salt.cc
@@ -336,7 +336,7 @@ class FileIoModule : public pp::Module {
 
 namespace pp {
 /// Factory function called by the browser when the module is first loaded.
-/// The browser keeps a singleton of this module.  It calls tihe
+/// The browser keeps a singleton of this module.  It calls the
 /// CreateInstance() method on the object you return to make instances.  There
 /// is one instance per <embed> tag on the page.  This is the main binding
 /// point for your NaCl module with the browser.
