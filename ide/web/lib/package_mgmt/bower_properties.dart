7cde6f4b0e1cb87804095d7f9fb0d8b87d30eef4
Sergey Shevchenko <sergeygs@google.com>
2014-10-21 12:47:40 -0700

    Read config from .bowerrc's in BowerProperties ctor (breaking; multi-part change)

--- ide/web/lib/package_mgmt/bower_properties.dart
+++ ide/web/lib/package_mgmt/bower_properties.dart
@@ -4,21 +4,51 @@
 
 library spark.package_mgmt.bower_properties;
 
+import 'dart:convert' show JSON;
+
 import 'package_manager.dart';
 import '../workspace.dart';
 
-// TODO(ussuri): Make package-private once no longer used outside.
-final BowerProperties bowerProperties = new BowerProperties();
-
 class BowerProperties extends PackageServiceProperties {
+  static const String _CONFIG_FILE = '.bowerrc';
+  static const Map<String, dynamic> _DEFAULT_CONFIG = const {
+    'directory': 'bower_components'
+  };
+
+  Map<String, dynamic> _config = {};
+
+  BowerProperties(Folder container) {
+    // Search for all .bowerrc config files from this folder upwards,
+    // merging them into _config in a descending order of preference.
+    while(container != null) {
+      File configFile = container.getChild(_CONFIG_FILE);
+      if (configFile != null) {
+        configFile.getContents().then((String text) {
+          Map<String, dynamic> newConfig = JSON.decode(text);
+          // Override possible duplicate keys in higher-level configs with
+          // lower-level ones.
+          newConfig.addAll(_config);
+          _config = newConfig;
+        }).catchError((e) {
+          if (e is FormatException) {
+            throw '$_CONFIG_FILE has invalid format: $e';
+          }
+        });
+      }
+      container = container.parent;
+    }
+    Map<String, dynamic> finalConfig = _DEFAULT_CONFIG;
+    finalConfig.addAll(_config);
+    _config = finalConfig;
+  }
+
   //
   // PackageServiceProperties virtual interface:
   //
 
   String get packageServiceName => 'bower';
   String get packageSpecFileName => 'bower.json';
-  // TODO(ussuri): Package name can be overridden in .bowerrc: handle that.
-  String get packagesDirName => 'bower_components';
+  String get packagesDirName => _config['directory'];
 
   // Bower doesn't use any of the below nullified properties/methods.
 
