096d6133f4a9894407edfbdc0e1ac30a27209099
Gaurav Agarwal <grv@grv-macbookair2.roam.corp.google.com>
2014-10-23 15:26:03 -0700

    Add missing constants file.

---
+++ ide/web/lib/git_salt/constants.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+library spark.gitsalt.constants;
+
+class GitSaltConstants {
+  static const int GIT_SALT_LOCAL_BRANCHES = 1;
+  static const int GIT_SALT_REMOTE_BRANCHES = 2;
+  static const int GIT_SALT_ALL_BRANCHES = 3;
+}
