8247c1f27ebef0b684fd70d31a3d0adf2d16cb0d
Gaurav Agarwal <grv@grv-macbookair2.roam.corp.google.com>
2014-10-23 15:14:43 -0700

    Add getBranches command.

--- ide/web/lib/git_salt/git_command.h
+++ ide/web/lib/git_salt/git_command.h
@@ -29,6 +29,17 @@ int parseString(pp::VarDictionary message, const char* name,
   option = var_option.AsString();
   return 0;
 }
+
+int parseInt(pp::VarDictionary message, const char* name,
+    int* option) {
+  pp::Var var_option = message.Get(name);
+  if (!var_option.is_int()) {
+    //TODO(grv): return error code;
+    return 1;
+  }
+  *option = var_option.AsInt();
+  return 0;
+}
 }
 
 
@@ -41,7 +52,6 @@ class GitSaltInstance;
 class GitCommand {
  protected:
   GitSaltInstance* _gitSalt;
-  std::string subject;
   pp::VarDictionary _args;
 
   int parseFileSystem(pp::VarDictionary message, std::string name,
@@ -51,6 +61,7 @@ class GitCommand {
   pp::FileSystem fileSystem;
   std::string fullPath;
   std::string url;
+  std::string subject;
   int error;
   git_repository*& repo;
 
@@ -58,7 +69,7 @@ class GitCommand {
              const std::string& subject,
              const pp::VarDictionary& args,
              git_repository*& repository)
-      : _gitSalt(git_salt), subject(subject), _args(args), repo(repository) {}
+      : _gitSalt(git_salt), _args(args), subject(subject), repo(repository) {}
 
   virtual int parseArgs();
   virtual int runCommand() = 0;
@@ -94,6 +105,22 @@ class GitCurrentBranch : public GitCommand {
 
  public:
   GitCurrentBranch(GitSaltInstance* git_salt,
+                   std::string subject,
+                   pp::VarDictionary args,
+                   git_repository*& repo)
+      : GitCommand(git_salt, subject, args, repo) {}
+
+  virtual int parseArgs();
+
+  int runCommand();
+};
+
+class GitGetBranches : public GitCommand {
+
+ public:
+  int flags;
+
+  GitGetBranches(GitSaltInstance* git_salt,
                    std::string subject,
                    pp::VarDictionary args,
                    git_repository*& repo)
