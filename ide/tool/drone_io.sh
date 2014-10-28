1e07e52502e9b5c9d082d6aa175c45cc0966db56
Devon Carew <devoncarew@google.com>
2014-10-28 05:07:38 -0700

    merge drone.sh fix to 0.17

--- ide/tool/drone_io.sh
+++ ide/tool/drone_io.sh
@@ -33,10 +33,11 @@ fi
 # Turn on fast fail for the bash script.
 set -e
 
-# Run tests the Dart version of the app.
-if [ "$HAS_DARTIUM" = "true" ]; then
-  #dart tool/test_runner.dart --dartium
-fi
+# Run tests on the Dart version of the app.
+#if [ "$HAS_DARTIUM" = "true" ]; then
+# TODO: Temporarily commented out until the Dartium / NaCl issue is sorted out.
+#  dart tool/test_runner.dart --dartium
+#fi
 
 # Run tests on the dart2js version of the app.
 if [ "$DRONE" = "true" ]; then
