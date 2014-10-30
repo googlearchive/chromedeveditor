adbad693efaaa5599d9a33c602900ee0f00076bd
Gaurav Agarwal <grv@chromium.org>
2014-10-30 15:15:41 -0700

    Re-enable build for daritum.

--- ide/tool/drone_io.sh
+++ ide/tool/drone_io.sh
@@ -34,10 +34,9 @@ fi
 set -e
 
 # Run tests on the Dart version of the app.
-#if [ "$HAS_DARTIUM" = "true" ]; then
-# TODO: Temporarily commented out until the Dartium / NaCl issue is sorted out.
-#  dart tool/test_runner.dart --dartium
-#fi
+if [ "$HAS_DARTIUM" = "true" ]; then
+  dart tool/test_runner.dart --dartium
+fi
 
 # Run tests on the dart2js version of the app.
 if [ "$DRONE" = "true" ]; then
