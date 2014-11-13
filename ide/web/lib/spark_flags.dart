a75a8536cde7eae1265e81edd197a229f85b58ae
Gaurav Agarwal <grv@grv-macbookpro.roam.corp.google.com>
2014-11-12 22:55:16 -0800

    Add new usb device api flag.

--- ide/web/lib/spark_flags.dart
+++ ide/web/lib/spark_flags.dart
@@ -7,6 +7,8 @@ library spark.flags;
 import 'dart:async';
 import 'dart:convert' show JSON;
 
+import 'platform_info.dart';
+
 /**
  * Stores global developer flags.
  */
@@ -29,7 +31,8 @@ class SparkFlags {
     "bower-override-dependencies": {
     },
     "bower-use-git-clone": false,
-    "package-files-are-editable": false
+    "package-files-are-editable": false,
+    "enable-new-usb-api": true
   };
 
   /**
@@ -73,6 +76,9 @@ class SparkFlags {
 
   static bool get polymerDesigner =>
       _flags['enable-polymer-designer'] == true;
+
+  static bool get enableNewUsbApi =>
+      _flags['enable-new-usb-api'] == true && PlatformInfo.chromeVersion >= 40;
 
   /**
    * Add new flags to the set, possibly overwriting the existing values.
